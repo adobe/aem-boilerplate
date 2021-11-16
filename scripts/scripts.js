491d2d638f4f078f76836176ac003c1ac00fc570
Alexandre Capt <acapt@adobe.com>
2021-11-16 10:42:45 +0100

    feat: align loading sequence to latest "standard"

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -83,18 +83,28 @@ function wrapSections($sections) {
 
 /**
  * Decorates a block.
- * @param {Element} $block The block element
+ * @param {Element} block The block element
  */
-export function decorateBlock($block) {
-  const classes = Array.from($block.classList.values());
-  const blockName = classes[0];
+export function decorateBlock(block) {
+  const classes = Array.from(block.classList.values());
+  let blockName = classes[0];
   if (!blockName) return;
-  const $section = $block.closest('.section-wrapper');
-  if ($section) {
-    $section.classList.add(`${blockName}-container`.replace(/--/g, '-'));
+  const section = block.closest('.section-wrapper');
+  if (section) {
+    section.classList.add(`${blockName}-container`.replace(/--/g, '-'));
   }
-  $block.classList.add('block');
-  $block.setAttribute('data-block-name', blockName);
+  const blocksWithVariants = ['recommended-articles'];
+  blocksWithVariants.forEach((b) => {
+    if (blockName.startsWith(`${b}-`)) {
+      const options = blockName.substring(b.length + 1).split('-').filter((opt) => !!opt);
+      blockName = b;
+      block.classList.add(b);
+      block.classList.add(...options);
+    }
+  });
+
+  block.classList.add('block');
+  block.setAttribute('data-block-name', blockName);
 }
 
 /**
@@ -105,6 +115,37 @@ function decorateBlocks($main) {
   $main
     .querySelectorAll('div.section-wrapper > div > div')
     .forEach(($block) => decorateBlock($block));
+}
+
+/**
+ * Builds a block DOM Element from a two dimensional array
+ * @param {string} blockName name of the block
+ * @param {any} content two dimensional array or string or object of content
+ */
+function buildBlock(blockName, content) {
+  const table = Array.isArray(content) ? content : [[content]];
+  const blockEl = document.createElement('div');
+  // build image block nested div structure
+  blockEl.classList.add(blockName);
+  table.forEach((row) => {
+    const rowEl = document.createElement('div');
+    row.forEach((col) => {
+      const colEl = document.createElement('div');
+      const vals = col.elems ? col.elems : [col];
+      vals.forEach((val) => {
+        if (val) {
+          if (typeof val === 'string') {
+            colEl.innerHTML += val;
+          } else {
+            colEl.appendChild(val);
+          }
+        }
+      });
+      rowEl.appendChild(colEl);
+    });
+    blockEl.appendChild(rowEl);
+  });
+  return (blockEl);
 }
 
 /**
@@ -173,88 +214,57 @@ export function readBlockConfig($block) {
 }
 
 /**
- * Official Google WEBP detection.
- * @param {Function} callback The callback function
+ * Returns a picture element with webp and fallbacks
+ * @param {string} src The image URL
+ * @param {boolean} eager load image eager
+ * @param {Array} breakpoints breakpoints and corresponding params (eg. width)
  */
-function checkWebpFeature(callback) {
-  const webpSupport = sessionStorage.getItem('webpSupport');
-  if (!webpSupport) {
-    const kTestImages = 'UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA';
-    const img = new Image();
-    img.onload = () => {
-      const result = (img.width > 0) && (img.height > 0);
-      window.webpSupport = result;
-      sessionStorage.setItem('webpSupport', result);
-      callback();
-    };
-    img.onerror = () => {
-      sessionStorage.setItem('webpSupport', false);
-      window.webpSupport = false;
-      callback();
-    };
-    img.src = `data:image/webp;base64,${kTestImages}`;
-  } else {
-    window.webpSupport = (webpSupport === 'true');
-    callback();
-  }
-}
-
-/**
- * Returns an image URL with optimization parameters
- * @param {string} url The image URL
- */
-export function getOptimizedImageURL(src) {
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 400px)', width: '2000' }, { width: '750' }]) {
   const url = new URL(src, window.location.href);
-  let result = src;
-  const { pathname, search } = url;
-  if (pathname.includes('media_')) {
-    const usp = new URLSearchParams(search);
-    usp.delete('auto');
-    if (!window.webpSupport) {
-      if (pathname.endsWith('.png')) {
-        usp.set('format', 'png');
-      } else if (pathname.endsWith('.gif')) {
-        usp.set('format', 'gif');
-      } else {
-        usp.set('format', 'pjpg');
-      }
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      picture.appendChild(source);
     } else {
-      usp.set('format', 'webply');
+      const img = document.createElement('img');
+      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
     }
-    result = `${src.split('?')[0]}?${usp.toString()}`;
-  }
-  return (result);
+  });
+
+  return picture;
 }
 
 /**
- * Resets an elelemnt's attribute to the optimized image URL.
- * @see getOptimizedImageURL
- * @param {Element} $elem The element
- * @param {string} attrib The attribute
+ * Removes formatting from images.
+ * @param {Element} main The container element
  */
-function resetOptimizedImageURL($elem, attrib) {
-  const src = $elem.getAttribute(attrib);
-  if (src) {
-    const oSrc = getOptimizedImageURL(src);
-    if (oSrc !== src) {
-      $elem.setAttribute(attrib, oSrc);
-    }
-  }
-}
-
-/**
- * WEBP Polyfill for older browser versions.
- * @param {Element} $elem The container element
- */
-export function webpPolyfill($elem) {
-  if (!window.webpSupport) {
-    $elem.querySelectorAll('img').forEach(($img) => {
-      resetOptimizedImageURL($img, 'src');
-    });
-    $elem.querySelectorAll('picture source').forEach(($source) => {
-      resetOptimizedImageURL($source, 'srcset');
-    });
-  }
+function removeStylingFromImages(main) {
+  // remove styling from images, if any
+  const imgs = [...main.querySelectorAll('strong picture'), ...main.querySelectorAll('em picture')];
+  imgs.forEach((img) => {
+    const parentEl = img.closest('p');
+    parentEl.prepend(img);
+    parentEl.lastChild.remove();
+  });
 }
 
 /**
@@ -286,15 +296,66 @@ export function normalizeHeadings($elem, allowedHeadings) {
 }
 
 /**
- * Decorates the main element.
- * @param {Element} $main The main element
+ * Decorates the picture elements.
+ * @param {Element} main The container element
  */
-export function decorateMain($main) {
-  wrapSections($main.querySelectorAll(':scope > div'));
-  checkWebpFeature(() => {
-    webpPolyfill($main);
+function decoratePictures(main) {
+  main.querySelectorAll('img[src*="/media_"').forEach((img, i) => {
+    const newPicture = createOptimizedPicture(img.src, img.alt, !i);
+    const picture = img.closest('picture');
+    if (picture) picture.parentElement.replaceChild(newPicture, picture);
   });
-  decorateBlocks($main);
+}
+
+/**
+ * returns an image caption of a picture elements
+ * @param {Element} picture picture element
+ */
+function getImageCaption(picture) {
+  const parentEl = picture.parentNode;
+  const parentSiblingEl = parentEl.nextElementSibling;
+  return (parentSiblingEl && parentSiblingEl.firstChild.nodeName === 'EM' ? parentSiblingEl : undefined);
+}
+
+/**
+ * builds images blocks from default content.
+ * @param {Element} main The container element
+ */
+function buildImageBlocks(main) {
+  // select all non-featured, default (non-images block) images
+  const imgs = [...main.querySelectorAll(':scope > div > p > picture')];
+  imgs.forEach((img) => {
+    const parent = img.parentNode;
+    const imagesBlock = buildBlock('images', {
+      elems: [parent.cloneNode(true), getImageCaption(img)],
+    });
+    parent.parentNode.insertBefore(imagesBlock, parent);
+    parent.remove();
+  });
+}
+
+/**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+function buildAutoBlocks(main) {
+  removeStylingFromImages(main);
+  try {
+    buildImageBlocks(main);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
+
+/**
+ * Removes the empty sections from the container element.
+ * @param {Element} main The container element
+ */
+function removeEmptySections(main) {
+  main.querySelectorAll(':scope > div:empty').forEach((div) => {
+    div.remove();
+  });
 }
 
 /**
@@ -302,59 +363,86 @@ export function decorateMain($main) {
  * @param {string} href The favicon URL
  */
 export function addFavIcon(href) {
-  const $link = document.createElement('link');
-  $link.rel = 'icon';
-  $link.type = 'image/svg+xml';
-  $link.href = href;
-  const $existingLink = document.querySelector('head link[rel="icon"]');
-  if ($existingLink) {
-    $existingLink.parentElement.replaceChild($link, $existingLink);
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/svg+xml';
+  link.href = href;
+  const existingLink = document.querySelector('head link[rel="icon"]');
+  if (existingLink) {
+    existingLink.parentElement.replaceChild(link, existingLink);
   } else {
-    document.getElementsByTagName('head')[0].appendChild($link);
+    document.getElementsByTagName('head')[0].appendChild(link);
   }
 }
 
 /**
- * Sets the trigger for the LCP (Largest Contentful Paint) event.
- * @see https://web.dev/lcp/
- * @param {Document} doc The document
- * @param {Function} postLCP The callback function
+ * Decorates the main element.
+ * @param {Element} main The main element
  */
-function setLCPTrigger(doc, postLCP) {
-  const $lcpCandidate = doc.querySelector('main > div:first-of-type img');
-  if ($lcpCandidate) {
-    if ($lcpCandidate.complete) {
-      postLCP();
-    } else {
-      $lcpCandidate.addEventListener('load', () => {
-        postLCP();
-      });
-      $lcpCandidate.addEventListener('error', () => {
-        postLCP();
-      });
-    }
-  } else {
-    postLCP();
+export function decorateMain(main) {
+  // forward compatible pictures redecoration
+  decoratePictures(main);
+  buildAutoBlocks(main);
+  removeEmptySections(main);
+  wrapSections(main.querySelectorAll(':scope > div'));
+  decorateBlocks(main);
+}
+
+const LCP_BLOCKS = []; // add your LCP blocks to the list
+
+/**
+ * loads everything needed to get to LCP.
+ */
+async function loadEager(doc) {
+  const main = doc.querySelector('main');
+  if (main) {
+    decorateMain(main);
+    doc.querySelector('body').classList.add('appear');
+
+    const block = doc.querySelector('.block');
+    const hasLCPBlock = (block && LCP_BLOCKS.includes(block.getAttribute('data-block-name')));
+    if (hasLCPBlock) await loadBlock(block, true);
+    const lcpCandidate = doc.querySelector('main img');
+    const loaded = {
+      then: (resolve) => {
+        if (lcpCandidate && !lcpCandidate.complete) {
+          lcpCandidate.addEventListener('load', () => resolve());
+          lcpCandidate.addEventListener('error', () => resolve());
+        } else {
+          resolve();
+        }
+      },
+    };
+    await loaded;
   }
+}
+
+/**
+ * loads everything that doesn't need to be delayed.
+ */
+async function loadLazy(doc) {
+  const main = doc.querySelector('main');
+
+  loadBlocks(main);
+  loadCSS('/styles/lazy-styles.css');
+  addFavIcon('/styles/favicon.svg');
+}
+
+/**
+ * loads everything that happens a lot later, without impacting
+ * the user experience.
+ */
+function loadDelayed() {
+  // load anything that can be postponed to the latest here
 }
 
 /**
  * Decorates the page.
- * @param {Window} win The window
  */
-async function decoratePage(win = window) {
-  const doc = win.document;
-  const $main = doc.querySelector('main');
-  if ($main) {
-    decorateMain($main);
-    doc.querySelector('body').classList.add('appear');
-    setLCPTrigger(doc, async () => {
-      // post LCP actions go here
-      await loadBlocks($main);
-      loadCSS('/styles/lazy-styles.css');
-      addFavIcon('/favicon.svg');
-    });
-  }
+async function decoratePage(doc) {
+  await loadEager(doc);
+  loadLazy(doc);
+  loadDelayed(doc);
 }
 
-decoratePage(window);
+decoratePage(document);

f1c2dc5d8c368cf67b2b2c690812e0cacf15b233
David Nuescheler <david@nuescheler.ch>
2022-01-05 09:13:12 -0800

    feat: default styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -444,7 +444,9 @@ function buildHeroBlock(main) {
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    h1.parentNode.prepend(buildBlock('hero', { elems: [picture, h1] }));
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    main.prepend(section);
   }
 }
 

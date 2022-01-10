f5ca47186f8c7f4d372df8633f6ed1462d4e67ab
David Nuescheler <david@nuescheler.ch>
2022-01-10 11:12:41 -0800

    feat: add footer

---
+++ blocks/footer/footer.js
@@ -0,0 +1,7 @@
+export default async function decorate(block) {
+  const resp = await fetch ('/footer.plain.html');
+  const html = await resp.text();
+  const footer = document.createElement('div');
+  footer.innerHTML = html;
+  block.append(footer);
+}

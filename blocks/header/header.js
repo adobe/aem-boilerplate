ecd22edf07a12ed5bc209acbc200a76c6e8eea7a
David Nuescheler <david@nuescheler.ch>
2022-05-02 10:33:07 -0600

    chore: better guards

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -23,41 +23,45 @@ export default async function decorate(block) {
   // fetch nav content
   const navPath = cfg.nav || '/nav-new';
   const resp = await fetch(`${navPath}.plain.html`);
-  const html = await resp.text();
+  if (resp.status === 200) {
+    const html = await resp.text();
 
-  // decorate nav DOM
-  const nav = document.createElement('nav');
-  nav.innerHTML = html;
-  decorateIcons(nav);
-  makeLinksRelative(nav);
+    // decorate nav DOM
+    const nav = document.createElement('nav');
+    nav.innerHTML = html;
+    decorateIcons(nav);
+    makeLinksRelative(nav);
 
-  const classes = ['brand', 'sections', 'tools'];
-  classes.forEach((e, j) => {
-    nav.children[j].classList.add(`nav-${e}`);
-  });
-
-  const navSections = [...nav.children][1];
-
-  navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-    if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-    navSection.addEventListener('click', () => {
-      const expanded = navSection.getAttribute('aria-expanded') === 'true';
-      collapseAllNavSections(navSections);
-      navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+    const classes = ['brand', 'sections', 'tools'];
+    classes.forEach((e, j) => {
+      const section = nav.children[j];
+      if (section) section.classList.add(`nav-${e}`);
     });
-  });
 
-  // hamburger for mobile
-  const hamburger = document.createElement('div');
-  hamburger.classList.add('nav-hamburger');
-  hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-  hamburger.addEventListener('click', () => {
-    const expanded = nav.getAttribute('aria-expanded') === 'true';
-    document.body.style.overflowY = expanded ? '' : 'hidden';
-    nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  });
-  nav.prepend(hamburger);
-  nav.setAttribute('aria-expanded', 'false');
-  decorateIcons(nav);
-  block.append(nav);
+    const navSections = [...nav.children][1];
+    if (navSections) {
+      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+        navSection.addEventListener('click', () => {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          collapseAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        });
+      });
+    }
+
+    // hamburger for mobile
+    const hamburger = document.createElement('div');
+    hamburger.classList.add('nav-hamburger');
+    hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+    hamburger.addEventListener('click', () => {
+      const expanded = nav.getAttribute('aria-expanded') === 'true';
+      document.body.style.overflowY = expanded ? '' : 'hidden';
+      nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+    });
+    nav.prepend(hamburger);
+    nav.setAttribute('aria-expanded', 'false');
+    decorateIcons(nav);
+    block.append(nav);
+  }
 }

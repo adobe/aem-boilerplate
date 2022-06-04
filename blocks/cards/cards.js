0727913510dfafb53a9e6ce898bf1376888de8e9
David Nuescheler <david@nuescheler.ch>
2022-06-04 11:10:35 -0600

    chore: simplify

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,11 @@
-export default function decorate(block, blockName) {
-  /* apply classes to elements */
-  const applyClasses = (elements, classNames) => {
-    elements.forEach((cell, i) => {
-      cell.className = classNames[i] ? `${blockName}-${classNames[i]}` : '';
-    });
-  };
-
+export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
-    applyClasses([...li.children], ['image', 'body']);
+    if (li.children.length === 1 && li.querySelector('picture')) li.className = 'cards-card-image';
+    else li.className('cards-card-body');
     ul.append(li);
   });
   block.textContent = '';

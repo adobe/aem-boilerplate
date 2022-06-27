36243b7cdda0f77bf5b378afb49dee6149c0c996
Lars Trieloff <lars@trieloff.net>
2022-06-27 16:02:31 +0200

    refactor(rum): move advanced rum features into separate script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,6 +4,4 @@ import { sampleRUM } from './scripts.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-console.log('sampleRUM delayed ID', sampleRUM.id);
-
 // add more delayed functionality here
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,8 +17,20 @@
  */
 
 export function sampleRUM(checkpoint, data = {}) {
-  this.id = Math.random();
-  console.log('sampleRUM ID', this.id);
+  sampleRUM.defer = sampleRUM.defer || [];
+  const defer = (fnname) =>
+    sampleRUM[fnname] = sampleRUM[fnname]
+    || ((...args) => sampleRUM.defer.push({ fnname, args }));
+  sampleRUM.drain = sampleRUM.drain ||
+    ((fnname, fn) => {
+      sampleRUM[fnname] = fn;
+      sampleRUM.defer
+        .filter(({ fnname: dfnname }) => dfnname === fnname)
+        .forEach(({ fnname, args }) => sampleRUM[fnname](...args));
+    });
+  sampleRUM.on = (chkpnt, fn) => sampleRUM.cases[chkpnt] = fn;
+  defer('observe');
+  defer('cwv');
   try {
     window.hlx = window.hlx || {};
     if (!window.hlx.rum) {
@@ -30,99 +42,35 @@ export function sampleRUM(checkpoint, data = {}) {
       const random = Math.random();
       const isSelected = (random * weight < 1);
       // eslint-disable-next-line object-curly-newline
-      window.hlx.rum = { weight, id, random, isSelected };
+      window.hlx.rum = { weight, id, random, isSelected, sampleRUM };
     }
-    const { random, weight, id } = window.hlx.rum;
-    if (random && (random * weight < 1)) {
-      const sendPing = () => {
+    const { weight, id } = window.hlx.rum;
+    if (window.hlx && window.hlx.rum && window.hlx.rum.isSelected) {
+      const sendPing = (data = data) => {
         // eslint-disable-next-line object-curly-newline, max-len, no-use-before-define
         const body = JSON.stringify({ weight, id, referer: window.location.href, generation: RUM_GENERATION, checkpoint, ...data });
         const url = `https://rum.hlx.page/.rum/${weight}`;
         // eslint-disable-next-line no-unused-expressions
         navigator.sendBeacon(url, body);
+        console.debug('ping:' + checkpoint, data);
       };
-      sendPing();
-      // special case CWV
-      if (checkpoint === 'cwv') {
-        // use classic script to avoid CORS issues
-        const script = document.createElement('script');
-        script.src = 'https://rum.hlx.page/.rum/web-vitals/dist/web-vitals.iife.js';
-        script.onload = () => {
-          const storeCWV = (measurement) => {
-            data.cwv = {};
-            data.cwv[measurement.name] = measurement.value;
-            sendPing();
-          };
-          // When loading `web-vitals` using a classic script, all the public
-          // methods can be found on the `webVitals` global namespace.
-          window.webVitals.getCLS(storeCWV);
-          window.webVitals.getFID(storeCWV);
-          window.webVitals.getLCP(storeCWV);
-        };
-        document.head.appendChild(script);
-      }
+      sampleRUM.cases = sampleRUM.cases || {
+        cwv: () => sampleRUM.cwv(sendPing) || true,
+        load: () => {
+          // use classic script to avoid CORS issues
+          const script = document.createElement('script');
+          script.src = 'http://localhost:3001/scripts/advancedrum.js';
+          document.head.appendChild(script);
+          sendPing(data);
+          return true;
+        }
+      };
+      cases[checkpoint] && cases[checkpoint]() || sendPing(data);
     }
   } catch (error) {
     // something went wrong
   }
 }
-
-sampleRUM.blockobserver = (window.IntersectionObserver) ? new IntersectionObserver((entries) => {
-  entries
-    .filter((entry) => entry.isIntersecting)
-    .forEach((entry) => {
-      sampleRUM.blockobserver.unobserve(entry.target); // observe only once
-      const target = sampleRUM.targetselector(entry.target);
-      const source = sampleRUM.sourceselector(entry.target);
-      sampleRUM('viewblock', { target, source });
-    });
-}, { threshold: 0.25 }) : { observe: () => { } };
-
-sampleRUM.mediaobserver = (window.IntersectionObserver) ? new IntersectionObserver((entries) => {
-  entries
-    .filter((entry) => entry.isIntersecting)
-    .forEach((entry) => {
-      sampleRUM.mediaobserver.unobserve(entry.target); // observe only once
-      const target = sampleRUM.targetselector(entry.target);
-      const source = sampleRUM.sourceselector(entry.target);
-      sampleRUM('viewmedia', { target, source });
-    });
-}, { threshold: 0.25 }) : { observe: () => { } };
-
-sampleRUM.observe = ((elements) => {
-  elements.forEach((element) => {
-    if (element.tagName.toLowerCase() === 'img'
-      || element.tagName.toLowerCase() === 'video'
-      || element.tagName.toLowerCase() === 'audio'
-      || element.tagName.toLowerCase() === 'iframe') {
-      sampleRUM.mediaobserver.observe(element);
-    } else {
-      sampleRUM.blockobserver.observe(element);
-    }
-  });
-});
-
-sampleRUM.targetselector = (element) => {
-  let value = element.getAttribute('href') || element.currentSrc || element.getAttribute('src');
-  if (value && value.startsWith('https://')) {
-    // resolve relative links
-    value = new URL(value, window.location).href;
-  }
-  return value;
-};
-
-sampleRUM.sourceselector = (element) => {
-  if (element === document.body || element === document.documentElement || !element) {
-    return undefined;
-  }
-  if (element.id) {
-    return `#${element.id}`;
-  }
-  if (element.getAttribute('data-block-name')) {
-    return `.${element.getAttribute('data-block-name')}`;
-  }
-  return sampleRUM.sourceselector(element.parentElement);
-};
 
 /**
  * Loads a CSS file.
@@ -641,18 +589,14 @@ const ICON_ROOT = '/icons';
 
 sampleRUM('top');
 
+window.addEventListener('load', () => sampleRUM('load'));
+
 window.addEventListener('unhandledrejection', (event) => {
   sampleRUM('error', { source: event.reason.sourceURL, target: event.reason.line });
 });
 
 window.addEventListener('error', (event) => {
   sampleRUM('error', { source: event.filename, target: event.lineno });
-});
-
-window.addEventListener('load', () => sampleRUM('load'));
-
-document.addEventListener('click', (event) => {
-  sampleRUM('click', { target: sampleRUM.targetselector(event.target), source: sampleRUM.sourceselector(event.target) });
 });
 
 loadPage(document);

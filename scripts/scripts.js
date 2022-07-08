d71c46a57e9f6160a7477539e83adef2f1dff8f1
Lars Trieloff <lars@trieloff.net>
2022-07-08 12:49:45 +0200

    style(rum): refactor to avoid eslint violations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,16 +18,18 @@
 
 export function sampleRUM(checkpoint, data = {}) {
   sampleRUM.defer = sampleRUM.defer || [];
-  const defer = (fnname) => sampleRUM[fnname] = sampleRUM[fnname]
-    || ((...args) => sampleRUM.defer.push({ fnname, args }));
+  const defer = (fnname) => {
+    sampleRUM[fnname] = sampleRUM[fnname]
+      || ((...args) => sampleRUM.defer.push({ fnname, args }));
+  };
   sampleRUM.drain = sampleRUM.drain
-    || ((fnname, fn) => {
-      sampleRUM[fnname] = fn;
+    || ((dfnname, fn) => {
+      sampleRUM[dfnname] = fn;
       sampleRUM.defer
-        .filter(({ fnname: dfnname }) => dfnname === fnname)
+        .filter(({ fnname }) => dfnname === fnname)
         .forEach(({ fnname, args }) => sampleRUM[fnname](...args));
     });
-  sampleRUM.on = (chkpnt, fn) => sampleRUM.cases[chkpnt] = fn;
+  sampleRUM.on = (chkpnt, fn) => { sampleRUM.cases[chkpnt] = fn; };
   defer('observe');
   defer('cwv');
   try {
@@ -45,13 +47,14 @@ export function sampleRUM(checkpoint, data = {}) {
     }
     const { weight, id } = window.hlx.rum;
     if (window.hlx && window.hlx.rum && window.hlx.rum.isSelected) {
-      const sendPing = (data = data) => {
+      const sendPing = (pdata = data) => {
         // eslint-disable-next-line object-curly-newline, max-len, no-use-before-define
         const body = JSON.stringify({ weight, id, referer: window.location.href, generation: RUM_GENERATION, checkpoint, ...data });
         const url = `https://rum.hlx.page/.rum/${weight}`;
         // eslint-disable-next-line no-unused-expressions
         navigator.sendBeacon(url, body);
-        console.debug(`ping:${checkpoint}`, data);
+        // eslint-disable-next-line no-console
+        console.debug(`ping:${checkpoint}`, pdata);
       };
       sampleRUM.cases = sampleRUM.cases || {
         cwv: () => sampleRUM.cwv(data) || true,
@@ -65,7 +68,7 @@ export function sampleRUM(checkpoint, data = {}) {
         },
       };
       sendPing(data);
-      sampleRUM.cases[checkpoint] && sampleRUM.cases[checkpoint]();
+      if (sampleRUM.cases[checkpoint]) { sampleRUM.cases[checkpoint](); }
     }
   } catch (error) {
     // something went wrong

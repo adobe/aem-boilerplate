f5374b80d4949c4c3f77add15987a4ac404dc997
Douglas Ross <sachioross@gmail.com>
2022-08-10 17:31:07 -0400

    Incorporate sass tooling into development process

---
+++ sass-compile.js
@@ -0,0 +1,43 @@
+import sass from 'sass';
+import fs from 'fs';
+import path from 'path';
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const compileAndSave = async (sassFile) => {
+    const dest = sassFile.replace(path.extname(sassFile), ".css");
+
+    fs.writeFile(dest, sass.compile(sassFile).css, (err) => {
+        if (err) console.log(err);
+        console.log(`Compiled ${sassFile} to ${dest}`);
+    });
+}
+
+const processFiles = async (parent) => {
+    let files = await readdir(parent, { withFileTypes: true});
+    for (const file of files) {
+        console.log(`${file.name} with extension ${path.extname(file.name)}`);
+        if (file.isDirectory()) {
+            await processFiles(path.join(parent, file.name));
+        }
+        if (path.extname(file.name) === '.scss') {
+            await compileAndSave(path.join(parent, file.name));
+        }
+    }
+}
+
+for (const folder of ["styles","blocks"]) {
+    try {
+        await processFiles(path.join(__dirname, folder));
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+fs.watch('.', {recursive: true}, (eventType, fileName) => {
+    if (path.extname(fileName) === ".scss" && eventType === "change") {
+        compileAndSave(path.join(__dirname, fileName));
+    }
+})

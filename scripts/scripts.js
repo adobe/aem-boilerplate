b8daee9afc8258a0c0c1d4f0a658f870adfdf4fc
Lars Trieloff <lars@trieloff.net>
2022-08-16 12:31:02 +0200

    fix(rum): do not rely on timeouts, instead start rum observation after `lazy` checkpoint has passed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -653,8 +653,6 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  window.setTimeout(() => sampleRUM.observe(main.querySelectorAll('picture > img')), 1000);
 }
 
 /**
@@ -686,6 +684,8 @@ async function loadLazy(doc) {
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
   sampleRUM('lazy');
+  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
+  sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
 
 /**

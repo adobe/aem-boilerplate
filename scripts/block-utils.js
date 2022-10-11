200c4d2393071b70a94c86587c7173df532cf118
David Nuescheler <david@nuescheler.ch>
2022-10-10 18:04:26 -0700

    Update scripts/block-utils.js
    Co-authored-by: Tobias Bocanegra <tripodsan@users.noreply.github.com>

--- scripts/block-utils.js
+++ scripts/block-utils.js
@@ -530,8 +530,8 @@ export async function waitForLCP(lcpBlocks) {
   await new Promise((resolve) => {
     if (lcpCandidate && !lcpCandidate.complete) {
       lcpCandidate.setAttribute('loading', 'eager');
-      lcpCandidate.addEventListener('load', () => resolve());
-      lcpCandidate.addEventListener('error', () => resolve());
+      lcpCandidate.addEventListener('load', resolve);
+      lcpCandidate.addEventListener('error', resolve);
     } else {
       resolve();
     }

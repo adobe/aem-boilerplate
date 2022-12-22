32efa7958a7653797dd10d77fe3a16afad1c74de
Julien Ramboz <ramboz@adobe.com>
2022-12-22 11:52:30 +0100

    refactor: extract normalizer plugin

--- scripts/plugins/normalizer.js
+++ scripts/plugins/normalizer.js
@@ -37,5 +37,6 @@ export const api = {
  * Logic to execute in the pre lazy phase
  */
 export async function preLazy(doc, options) {
-  await normalizeHeadings(doc, options.allowedHeadings);
+  doc.normalize();
+  normalizeHeadings(doc, options.allowedHeadings);
 }

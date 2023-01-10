6781ad1fd144d26305f2eca2ddcdec2533da2f9d
Katie Sexton <ksexton@adobe.com>
2023-01-10 11:50:19 -0700

    fix: param type in buildBlock

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -312,7 +312,7 @@ export function decorateBlocks(main) {
 /**
  * Builds a block DOM Element from a two dimensional array, string, or object
  * @param {string} blockName name of the block
- * @param {any} content two dimensional array or string or object of content
+ * @param {*} content two dimensional array or string or object of content
  */
 export function buildBlock(blockName, content) {
   const table = Array.isArray(content) ? content : [[content]];

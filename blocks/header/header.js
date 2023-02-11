7be8fc7728bd534eb9ae727398a54c0dbcbccb6a
David Nuescheler <david@nuescheler.ch>
2023-02-11 15:42:07 -0700

    chore: better nav reload after publish

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -95,7 +95,7 @@ export default async function decorate(block) {
 
   // fetch nav content
   const navPath = config.nav || '/nav';
-  const resp = await fetch(`${navPath}.plain.html`);
+  const resp = await fetch(`${navPath}.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
     const html = await resp.text();

b478ec34a5d283f475146ad895178f682844e65d
Julien Ramboz <ramboz@users.noreply.github.com>
2023-02-28 07:38:25 -0800

    feat: auto-sprite svg icons (#149)
    ## Context
    See https://github.com/BambooHR/bamboohr-website/pull/75 for full context
    
    ## Problem
    On pages that massively use icons, the repeated loading of those via `fetch` requests delays the rendering of the lazy parts of the page (header, footer, etc.)
    
    ## The solution
    We create an inline SVG sprite in the document and dynamically populate it when we encounter new icons.
    
    <img width="2553" alt="Screenshot 2022-12-02 at 9 47 08 AM" src="https://user-images.githubusercontent.com/1235810/205252971-2fad6a8b-dc6a-41a3-b7f5-0af111347022.png">
    
    On the boilerplate that doesn't really use any icons, the change is essentially transparent.
    
    Fix https://github.com/BambooHR/bamboohr-website/pull/75
    
    Test URLs:
    - Before: https://main--helix-project-boilerplate--adobe.hlx.live/
    - After: https://inline-svg-sprite--helix-project-boilerplate--ramboz.hlx.live/

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -123,27 +123,64 @@ export function toCamelCase(name) {
   return toClassName(name).replace(/-([a-z])/g, (g) => g[1].toUpperCase());
 }
 
+const ICONS_CACHE = {};
 /**
  * Replace icons with inline SVG and prefix with codeBasePath.
  * @param {Element} element
  */
-export function decorateIcons(element = document) {
-  element.querySelectorAll('span.icon').forEach(async (span) => {
-    if (span.classList.length < 2 || !span.classList[1].startsWith('icon-')) {
-      return;
-    }
-    const icon = span.classList[1].substring(5);
-    // eslint-disable-next-line no-use-before-define
-    const resp = await fetch(`${window.hlx.codeBasePath}/icons/${icon}.svg`);
-    if (resp.ok) {
-      const iconHTML = await resp.text();
-      if (iconHTML.match(/<style/i)) {
-        const img = document.createElement('img');
-        img.src = `data:image/svg+xml,${encodeURIComponent(iconHTML)}`;
-        span.appendChild(img);
-      } else {
-        span.innerHTML = iconHTML;
+export async function decorateIcons(element) {
+  // Prepare the inline sprite
+  let svgSprite = document.getElementById('franklin-svg-sprite');
+  if (!svgSprite) {
+    const div = document.createElement('div');
+    div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id="franklin-svg-sprite" style="display: none"></svg>';
+    svgSprite = div.firstElementChild;
+    document.body.append(div.firstElementChild);
+  }
+
+  // Download all new icons
+  const icons = [...element.querySelectorAll('span.icon')];
+  await Promise.all(icons.map(async (span) => {
+    const iconName = Array.from(span.classList).find((c) => c.startsWith('icon-')).substring(5);
+    if (!ICONS_CACHE[iconName]) {
+      ICONS_CACHE[iconName] = true;
+      try {
+        const response = await fetch(`${window.hlx.codeBasePath}/icons/${iconName}.svg`);
+        if (!response.ok) {
+          ICONS_CACHE[iconName] = false;
+          return;
+        }
+        // Styled icons don't play nice with the sprite approach because of shadow dom isolation
+        const svg = await response.text();
+        if (svg.match(/(<style | class=)/)) {
+          ICONS_CACHE[iconName] = { styled: true, html: svg };
+        } else {
+          ICONS_CACHE[iconName] = {
+            html: svg
+              .replace('<svg', `<symbol id="icons-sprite-${iconName}"`)
+              .replace(/ width=".*?"/, '')
+              .replace(/ height=".*?"/, '')
+              .replace('</svg>', '</symbol>'),
+          };
+        }
+      } catch (err) {
+        ICONS_CACHE[iconName] = false;
+        console.error(err);
       }
+    }
+  }));
+
+  const symbols = Object.values(ICONS_CACHE).filter((v) => !v.styled).map((v) => v.html).join('\n');
+  svgSprite.innerHTML += symbols;
+
+  icons.forEach((span) => {
+    const iconName = Array.from(span.classList).find((c) => c.startsWith('icon-')).split('-')[1];
+    const parent = span.firstElementChild?.tagName === 'A' ? span.firstElementChild : span;
+    // Styled icons need to be inlined as-is, while unstyled ones can leverage the sprite
+    if (ICONS_CACHE[iconName].styled) {
+      parent.innerHTML = ICONS_CACHE[iconName].html;
+    } else {
+      parent.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"><use href="#${iconName}"/></svg>`;
     }
   });
 }

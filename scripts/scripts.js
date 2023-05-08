1b306a5e6dc074a743bf4f381a1864931a6c8b4c
David Nuescheler <david@nuescheler.ch>
2023-05-07 17:44:41 -0700

    chore: more realistic, working ootb favicon

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -79,13 +79,13 @@ async function loadEager(doc) {
 export function addFavIcon(href) {
   const link = document.createElement('link');
   link.rel = 'icon';
-  link.type = 'image/svg+xml';
+  link.type = 'image/png';
   link.href = href;
   const existingLink = document.querySelector('head link[rel="icon"]');
   if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
+    existingLink.replaceWith(link);
   } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
+    document.head.append(link);
   }
 }
 
@@ -105,7 +105,7 @@ async function loadLazy(doc) {
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
+  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.png`);
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));

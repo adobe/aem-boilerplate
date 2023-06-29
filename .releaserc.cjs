741e902eec4085d7102097a867523ef2b2344909
Lars Trieloff <lars@trieloff.net>
2023-06-29 12:43:53 +0200

    build(semantic-release): re-enable changelog plugin

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -2,9 +2,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     //"@semantic-release/release-notes-generator",
-    //["@semantic-release/changelog", {
-    //  "changelogFile": "CHANGELOG.md",
-    //}],
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }],
     ["@semantic-release/npm", {
       "publish": false,
     }],

675ebfc2a5f72401126f1fc7873137f40794038a
Lars Trieloff <lars@trieloff.net>
2023-06-29 12:34:15 +0200

    build(semantic-release): disable github plugin

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,7 +10,7 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    // ["@semantic-release/github", {}]
   ],
   branches: ['main'],
 };

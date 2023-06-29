a455fc808bab093d1e344e94373b97f0579b8ee3
Lars Trieloff <lars@trieloff.net>
2023-06-29 12:38:32 +0200

    build(semantic-release): write back to package.json

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -5,7 +5,9 @@ module.exports = {
     //["@semantic-release/changelog", {
     //  "changelogFile": "CHANGELOG.md",
     //}],
-    // "@semantic-release/npm",
+    ["@semantic-release/npm", {
+      "publish": false,
+    }],
     ["@semantic-release/git", {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"

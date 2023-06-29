3b6b3c4868d51a590b0f20ab1a98b4a44650a0ca
Lars Trieloff <lars@trieloff.net>
2023-06-29 12:10:48 +0200

    build(semantic-release): disable most plugins for now

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -2,15 +2,15 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog", {
-      "changelogFile": "CHANGELOG.md",
-    }],
+    //["@semantic-release/changelog", {
+    //  "changelogFile": "CHANGELOG.md",
+    //}],
     // "@semantic-release/npm",
     ["@semantic-release/git", {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    //["@semantic-release/github", {}]
   ],
   branches: ['main'],
 };

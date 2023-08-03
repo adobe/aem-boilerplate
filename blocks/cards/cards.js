fec6dac55c5e659ddad9aedb64d814bf5246ab7c
David Nuescheler <david@nuescheler.ch>
2023-08-02 20:17:31 -0600

    chore: fix cards to be fragment compatible

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,7 +5,7 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
+    while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';

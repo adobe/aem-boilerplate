5590bd04c26093074e8cdcb4a29f47cdd75c889a
Satya Deep Maheshwari <satyam@adobe.com>
2023-08-14 17:58:53 +0530

    Add webtools support

--- blocks/aem-asset-selector/aem-asset-selector-util.js
+++ blocks/aem-asset-selector/aem-asset-selector-util.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 const AS_MFE = 'https://experience.adobe.com/solutions/CQ-assets-selectors/assets/resources/asset-selectors.js';
 const IMS_ENV_STAGE = 'stg1';
 const IMS_ENV_PROD = 'prod';
 const API_KEY = 'franklin';
 const PROXY = 'https://html-transfer-cf-worker.satyadeep.workers.dev';
+const WEB_TOOLS = 'https://master-sacred-dove.ngrok-free.app';
 
 let imsInstance = null;
 let imsEnvironment;
@@ -55,6 +57,23 @@ function onClose() {
   // selectorDialog.close();
 }
 
+async function getAssetPublicUrl(url) {
+  const response = await fetch(`${WEB_TOOLS}/asset-bin?src=${url}`, {
+    headers: {
+      Authorization: `Bearer ${imsInstance.getImsToken()}`,
+      'x-api-key': API_KEY,
+    },
+  });
+  if (response && response.ok) {
+    const json = await response.json();
+    return json['asset-url'];
+  }
+  if (response && !response.ok) {
+    throw new Error(response.statusTest);
+  }
+  return null;
+}
+
 async function getAssetBlob(url) {
   let response;
   try {
@@ -87,16 +106,22 @@ async function handleSelection(selection) {
   let maxRendition = null;
   // eslint-disable-next-line no-underscore-dangle
   selectedAsset._links['http://ns.adobe.com/adobecloud/rel/rendition'].forEach((rendition) => {
-    if ((!maxRendition || maxRendition.width < rendition.width) && (rendition.type === 'image/png')) {
+    if ((!maxRendition || maxRendition.width < rendition.width)) {
       maxRendition = rendition;
     }
   });
-  const assetBlob = await getAssetBlob(maxRendition.href);
-  // Clear the clipboard
-  console.log('Clearing clipboard');
-  await navigator.clipboard.writeText('');
+  console.log('Selected rendition:', maxRendition.href);
+  // const assetBlob = await getAssetBlob(maxRendition.href);
+  // const assetMarkup = `<img src="${URL.createObjectURL(assetBlob)}" alt="${selectedAsset.name}" />`;
 
-  const data = [new ClipboardItem({ [assetBlob.type]: assetBlob })];
+  const assetPublicUrl = await getAssetPublicUrl(maxRendition.href.substring(0, maxRendition.href.indexOf('?')));
+  console.log('Asset public URL:', assetPublicUrl);
+  const assetMarkup = `<img src="${assetPublicUrl}"  />`;
+
+  const data = [
+    // eslint-disable-next-line no-undef
+    new ClipboardItem({ 'text/html': new Blob([assetMarkup], { type: 'text/html' }) }),
+  ];
   // Write the new clipboard contents
   await navigator.clipboard.write(data);
   // onClose();

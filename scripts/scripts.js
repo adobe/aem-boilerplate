aa0445174c9f854b894e51b27c909949b3a75d98
tmathern <60901087+tmathern@users.noreply.github.com>
2023-08-24 00:14:51 -0700

    Issue 50 - Remove header and footer (since they're not used) (#53)
    * Simplify code
    
    * Remove unused icons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,8 +13,6 @@ governing permissions and limitations under the License.
 import {
   sampleRUM,
   buildBlock,
-  loadHeader,
-  loadFooter,
   decorateButtons,
   decorateIcons,
   decorateSections,
@@ -116,9 +114,6 @@ async function loadLazy(doc) {
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
-
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();

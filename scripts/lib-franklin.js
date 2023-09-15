1dde0b735576214f45d0e883016a0a4cbe84f49e
Julien Ramboz <ramboz@adobe.com>
2023-09-14 18:28:31 -0700

    feat: replace complex spriting logic with plain async image icons

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -158,12 +158,11 @@ export function toCamelCase(name) {
 }
 
 /**
- * Replace icons with inline SVG and prefix with codeBasePath.
  * Add <img> for icon, prefixed with codeBasePath and optional prefix.
  * @param {span} [element] span element with icon classes
  * @param {string} [prefix] prefix to be added to icon the src
  */
-export async function decorateIcons(span, prefix = '') {
+export function decorateIcon(span, prefix = '') {
   const iconName = Array.from(span.classList).find((c) => c.startsWith('icon-')).substring(5);
   const img = document.createElement('img');
   img.dataset.iconName = iconName;
@@ -171,6 +170,18 @@ export async function decorateIcons(span, prefix = '') {
   img.loading = 'lazy';
 
   span.append(img);
+}
+
+/**
+ * Add <img> for icons, prefixed with codeBasePath and optional prefix.
+ * @param {Element} [element] Element containing icons
+ * @param {string} [prefix] prefix to be added to icon the src
+ */
+export function decorateIcons(element, prefix = '') {
+  const icons = [...element.querySelectorAll('span.icon')];
+  icons.forEach((span) => {
+    decorateIcon(span, prefix);
+  });
 }
 
 /**

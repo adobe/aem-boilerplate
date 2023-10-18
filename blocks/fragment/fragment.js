71604589592c63cbc563e69dad15e5f75f68869e
David Nuescheler <david@nuescheler.ch>
2023-10-18 12:29:27 -0700

    chore: reset base of media in fragments

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -10,7 +10,7 @@ import {
 
 import {
   loadBlocks,
-} from '../../scripts/lib-franklin.js';
+} from '../../scripts/aem.js';
 
 /**
  * Loads a fragment.
@@ -23,6 +23,16 @@ export async function loadFragment(path) {
     if (resp.ok) {
       const main = document.createElement('main');
       main.innerHTML = await resp.text();
+
+      // reset base path for media to fragment base
+      const resetAttributeBase = (tag, attr) => {
+        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+        });
+      };
+      resetAttributeBase('img', 'src');
+      resetAttributeBase('source', 'srcset');
+
       decorateMain(main);
       await loadBlocks(main);
       return main;

2c9e230b3c01a6df7e54524c755d0cee557d8181
Mark J. Becker <herzog31@users.noreply.github.com>
2023-11-03 17:01:50 +0100

    Add PDP drop-in

---
+++ blocks/product-details-custom/product-details.js
@@ -0,0 +1,201 @@
+/* eslint-disable class-methods-use-this */
+import {
+  Component, Fragment, h, render,
+} from '../../scripts/preact.js';
+
+import htm from '../../scripts/htm.js';
+import Carousel from './ProductDetailsCarousel.js';
+import Sidebar from './ProductDetailsSidebar.js';
+import ProductDetailsShimmer from './ProductDetailsShimmer.js';
+import {
+  getProduct,
+  getSkuFromUrl,
+  performCatalogServiceQuery,
+  refineProductQuery,
+  setJsonLd,
+} from '../../scripts/commerce.js';
+
+const html = htm.bind(h);
+
+export function errorGettingProduct(code = 404) {
+  fetch(`/${code}.html`).then((response) => {
+    if (response.ok) {
+      return response.text();
+    }
+    throw new Error(`Error getting ${code} page`);
+  }).then((htmlText) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlText, 'text/html');
+    document.body.innerHTML = doc.body.innerHTML;
+    document.head.innerHTML = doc.head.innerHTML;
+  });
+  document.body.innerHTML = '';
+}
+
+async function getVariantDetails(variantIds, sku) {
+  const result = await performCatalogServiceQuery(
+    refineProductQuery,
+    {
+      sku: sku.toUpperCase(),
+      variantIds,
+    },
+  );
+  return {
+    images: result.refineProduct?.images,
+    price: result.refineProduct?.price,
+  };
+}
+
+async function setJsonLdProduct(product) {
+  const {
+    name, inStock, description, sku, urlKey, price, priceRange, images, attributes,
+  } = product;
+  const amount = priceRange?.minimum?.final?.amount || price?.final?.amount;
+  const brand = attributes.find((attr) => attr.name === 'brand');
+
+  setJsonLd({
+    '@context': 'http://schema.org',
+    '@type': 'Product',
+    name,
+    description,
+    image: images[0]?.url,
+    offers: [{
+      '@type': 'http://schema.org/Offer',
+      price: amount?.value,
+      priceCurrency: amount?.currency,
+      availability: inStock ? 'http://schema.org/InStock' : 'http://schema.org/OutOfStock',
+    }],
+    productID: sku,
+    brand: {
+      '@type': 'Brand',
+      name: brand?.value,
+    },
+    url: new URL(`/products/${urlKey}/${sku.toLowerCase()}`, window.location),
+    sku,
+    '@id': new URL(`/products/${urlKey}/${sku.toLowerCase()}`, window.location),
+  }, 'product');
+}
+
+class ProductDetailPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+
+    this.onSelectionChanged = this.onSelectionChanged.bind(this);
+    this.onAddToCart = this.onAddToCart.bind(this);
+    this.onQuantityChanged = this.onQuantityChanged.bind(this);
+    this.onAddToWishlist = this.onAddToWishlist.bind(this);
+  }
+
+  async componentDidMount() {
+    const product = await getProduct(getSkuFromUrl());
+
+    if (!product) {
+      errorGettingProduct();
+    }
+
+    this.setState({
+      product,
+      loading: false,
+      selection: {},
+    });
+  }
+
+  onAddToCart = async () => {
+    if (Object.keys(this.state.selection).length === (this.state.product.options?.length || 0)) {
+      const optionsUIDs = Object.values(this.state.selection).map((option) => option.id);
+      const { cartApi } = await import('../../scripts/minicart/api.js');
+      console.debug('onAddToCart', {
+        sku: this.state.product.sku, optionsUIDs, quantity: this.state.selectedQuantity ?? 1,
+      });
+      cartApi.addToCart(this.state.product.sku, optionsUIDs, this.state.selectedQuantity ?? 1);
+    }
+  };
+
+  onAddToWishlist = async () => {
+    console.debug('onAddToWishlist', this.state.product.sku);
+  };
+
+  onQuantityChanged = (quantity) => {
+    this.setState({ selectedQuantity: quantity });
+  };
+
+  onSelectionChanged = (fragment) => {
+    // update selection value
+    this.setState((oldState) => ({
+      selection: {
+        ...oldState.selection,
+        ...fragment,
+      },
+    }));
+
+    // fetch new images and prices
+    const variantIds = Object.values({ ...this.state.selection, ...fragment })
+      .map((selection) => selection.id);
+    getVariantDetails(variantIds, this.state.product.sku).then(({ images, price }) => {
+      this.setState((oldState) => ({
+        product: {
+          ...oldState.product,
+          images,
+          price,
+        },
+      }));
+    });
+  };
+
+  async componentDidUpdate() {
+    const { loading, product } = this.state;
+    if (!loading && product) {
+      setJsonLdProduct(product);
+      /* getMagentoStorefrontEvents(async (mse) => {
+        const sku = mse.context.getProduct()?.sku;
+        if (!sku) {
+          mse.context.setProduct({
+            ...mse.context.getProduct(),
+            ...product,
+          });
+        }
+      }); */
+    }
+  }
+
+  render() {
+    if (this.state.loading) {
+      return html`<${ProductDetailsShimmer} />`;
+    }
+
+    return html`
+      <${Fragment} >
+        <${Carousel} product=${this.state.product} />
+        <${Sidebar}
+          product=${this.state.product}
+          selection=${this.state.selection}
+          onSelectionChanged=${this.onSelectionChanged}
+          onAddToCart=${this.onAddToCart}
+          onAddToWishlist=${this.onAddToWishlist}
+          onQuantityChanged=${this.onQuantityChanged}
+        />
+        <div class="product-detail-description">
+          <h3>PRODUCT DETAILS</h3>
+          <div dangerouslySetInnerHTML=${{ __html: this.state.product.description }}></div>
+        </div>
+      <//>
+    `;
+  }
+}
+
+export default async function decorate($block) {
+  $block.innerHTML = '<div class="full-height"></div>';
+
+  const skuFromUrl = getSkuFromUrl();
+  if (!skuFromUrl) {
+    errorGettingProduct();
+  }
+
+  const app = html`<${ProductDetailPage} sku=${skuFromUrl} />`;
+
+  render(app, $block);
+}
--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -1,201 +1,58 @@
-/* eslint-disable class-methods-use-this */
-import {
-  Component, Fragment, h, render,
-} from '../../scripts/preact.js';
+/* eslint-disable import/no-unresolved */
+import { initializers } from '@dropins/elsie/initializer.js';
+import * as productApi from '@dropins/storefront-pdp/api.js';
+import ProductDetails from '@dropins/storefront-pdp/containers/ProductDetails.js';
+import { render as productRenderer } from '@dropins/storefront-pdp/render.js';
+import { getConfigValue } from '../../scripts/configs.js';
+import { getSkuFromUrl } from '../../scripts/commerce.js';
 
-import htm from '../../scripts/htm.js';
-import Carousel from './ProductDetailsCarousel.js';
-import Sidebar from './ProductDetailsSidebar.js';
-import ProductDetailsShimmer from './ProductDetailsShimmer.js';
-import {
-  getProduct,
-  getSkuFromUrl,
-  performCatalogServiceQuery,
-  refineProductQuery,
-  setJsonLd,
-} from '../../scripts/commerce.js';
+export default async function decorate(block) {
+  const commerceEndpoint = await getConfigValue('commerce-endpoint');
+  productApi.setEndpoint(commerceEndpoint);
 
-const html = htm.bind(h);
-
-export function errorGettingProduct(code = 404) {
-  fetch(`/${code}.html`).then((response) => {
-    if (response.ok) {
-      return response.text();
-    }
-    throw new Error(`Error getting ${code} page`);
-  }).then((htmlText) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlText, 'text/html');
-    document.body.innerHTML = doc.body.innerHTML;
-    document.head.innerHTML = doc.head.innerHTML;
+  productApi.setFetchGraphQlHeaders({
+    'Content-Type': 'application/json',
+    'Magento-Environment-Id': await getConfigValue('commerce-environment-id'),
+    'Magento-Website-Code': await getConfigValue('commerce-website-code'),
+    'Magento-Store-View-Code': await getConfigValue('commerce-store-view-code'),
+    'Magento-Store-Code': await getConfigValue('commerce-store-code'),
+    'Magento-Customer-Group': await getConfigValue('commerce-customer-group'),
+    'x-api-key': await getConfigValue('commerce-x-api-key'),
   });
-  document.body.innerHTML = '';
-}
+  initializers.register(productApi.initialize);
 
-async function getVariantDetails(variantIds, sku) {
-  const result = await performCatalogServiceQuery(
-    refineProductQuery,
-    {
-      sku: sku.toUpperCase(),
-      variantIds,
-    },
-  );
-  return {
-    images: result.refineProduct?.images,
-    price: result.refineProduct?.price,
-  };
-}
+  const heading = document.createElement('h2');
+  heading.classList.add('pdp-product__heading');
+  heading.textContent = 'product description';
 
-async function setJsonLdProduct(product) {
-  const {
-    name, inStock, description, sku, urlKey, price, priceRange, images, attributes,
-  } = product;
-  const amount = priceRange?.minimum?.final?.amount || price?.final?.amount;
-  const brand = attributes.find((attr) => attr.name === 'brand');
+  productRenderer.render(ProductDetails, {
+    sku: getSkuFromUrl(),
+    hideQuantity: false,
+    slots: {
+      Actions: (ctx) => {
+        // Add to Cart Button
+        ctx.appendButton({
+          text: 'Add to Cart',
+          icon: 'cart',
+          variant: 'primary',
+          onClick: async () => {
+            const { cartApi } = await import('../../scripts/minicart/api.js');
+            const { sku, options = [], quantity } = ctx.values;
+            console.debug('Add to Cart', sku, options, quantity);
+            cartApi.addToCart(sku, options, quantity);
+          },
+        });
 
-  setJsonLd({
-    '@context': 'http://schema.org',
-    '@type': 'Product',
-    name,
-    description,
-    image: images[0]?.url,
-    offers: [{
-      '@type': 'http://schema.org/Offer',
-      price: amount?.value,
-      priceCurrency: amount?.currency,
-      availability: inStock ? 'http://schema.org/InStock' : 'http://schema.org/OutOfStock',
-    }],
-    productID: sku,
-    brand: {
-      '@type': 'Brand',
-      name: brand?.value,
-    },
-    url: new URL(`/products/${urlKey}/${sku.toLowerCase()}`, window.location),
-    sku,
-    '@id': new URL(`/products/${urlKey}/${sku.toLowerCase()}`, window.location),
-  }, 'product');
-}
+        // Add to Wishlist Button
+        ctx.appendButton({
+          icon: 'Heart',
+          variant: 'primary',
+          onClick: () => console.debug('Add to Wishlist', ctx.data),
+        });
 
-class ProductDetailPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true,
-    };
-
-    this.onSelectionChanged = this.onSelectionChanged.bind(this);
-    this.onAddToCart = this.onAddToCart.bind(this);
-    this.onQuantityChanged = this.onQuantityChanged.bind(this);
-    this.onAddToWishlist = this.onAddToWishlist.bind(this);
-  }
-
-  async componentDidMount() {
-    const product = await getProduct(getSkuFromUrl());
-
-    if (!product) {
-      errorGettingProduct();
-    }
-
-    this.setState({
-      product,
-      loading: false,
-      selection: {},
-    });
-  }
-
-  onAddToCart = async () => {
-    if (Object.keys(this.state.selection).length === (this.state.product.options?.length || 0)) {
-      const optionsUIDs = Object.values(this.state.selection).map((option) => option.id);
-      const { cartApi } = await import('../../scripts/minicart/api.js');
-      console.debug('onAddToCart', {
-        sku: this.state.product.sku, optionsUIDs, quantity: this.state.selectedQuantity ?? 1,
-      });
-      cartApi.addToCart(this.state.product.sku, optionsUIDs, this.state.selectedQuantity ?? 1);
-    }
-  };
-
-  onAddToWishlist = async () => {
-    console.debug('onAddToWishlist', this.state.product.sku);
-  };
-
-  onQuantityChanged = (quantity) => {
-    this.setState({ selectedQuantity: quantity });
-  };
-
-  onSelectionChanged = (fragment) => {
-    // update selection value
-    this.setState((oldState) => ({
-      selection: {
-        ...oldState.selection,
-        ...fragment,
+        // Add Product Details Description header
+        ctx.appendHTMLElement(heading);
       },
-    }));
-
-    // fetch new images and prices
-    const variantIds = Object.values({ ...this.state.selection, ...fragment })
-      .map((selection) => selection.id);
-    getVariantDetails(variantIds, this.state.product.sku).then(({ images, price }) => {
-      this.setState((oldState) => ({
-        product: {
-          ...oldState.product,
-          images,
-          price,
-        },
-      }));
-    });
-  };
-
-  async componentDidUpdate() {
-    const { loading, product } = this.state;
-    if (!loading && product) {
-      setJsonLdProduct(product);
-      /* getMagentoStorefrontEvents(async (mse) => {
-        const sku = mse.context.getProduct()?.sku;
-        if (!sku) {
-          mse.context.setProduct({
-            ...mse.context.getProduct(),
-            ...product,
-          });
-        }
-      }); */
-    }
-  }
-
-  render() {
-    if (this.state.loading) {
-      return html`<${ProductDetailsShimmer} />`;
-    }
-
-    return html`
-      <${Fragment} >
-        <${Carousel} product=${this.state.product} />
-        <${Sidebar}
-          product=${this.state.product}
-          selection=${this.state.selection}
-          onSelectionChanged=${this.onSelectionChanged}
-          onAddToCart=${this.onAddToCart}
-          onAddToWishlist=${this.onAddToWishlist}
-          onQuantityChanged=${this.onQuantityChanged}
-        />
-        <div class="product-detail-description">
-          <h3>PRODUCT DETAILS</h3>
-          <div dangerouslySetInnerHTML=${{ __html: this.state.product.description }}></div>
-        </div>
-      <//>
-    `;
-  }
-}
-
-export default async function decorate($block) {
-  $block.innerHTML = '<div class="full-height"></div>';
-
-  const skuFromUrl = getSkuFromUrl();
-  if (!skuFromUrl) {
-    errorGettingProduct();
-  }
-
-  const app = html`<${ProductDetailPage} sku=${skuFromUrl} />`;
-
-  render(app, $block);
+    },
+  })(block);
 }

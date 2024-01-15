e3510435fa66c431c78953681ef715a83d52c661
Dirk <drudolph@adobe.com>
2024-01-15 11:22:00 +0100

    fix: preserve card attributes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,7 +5,9 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    [...row.attributes].forEach(({ attrName, attrValue }) => li.setAttribute(attrName, attrValue));
+    [...row.attributes].forEach(({ nodeName, nodeValue }) => {
+      li.setAttribute(nodeName, nodeValue);
+    });
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';

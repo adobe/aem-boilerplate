94847f9de1424866f7793331c9cb37e2a1d4c083
Mark J. Becker <herzog31@users.noreply.github.com>
2024-01-15 14:29:33 +0100

    Use global design tokens

--- blocks/commerce-order-confirmation/commerce-order-confirmation.js
+++ blocks/commerce-order-confirmation/commerce-order-confirmation.js
@@ -1,9 +1,9 @@
 import { setEndpoint } from '@dropins/elsie/fetch-graphql.js';
 import { initializers } from '@dropins/elsie/initializer.js';
-import { getConfigValue } from '../../scripts/configs.js';
 import * as orderConfirmationApi from '@dropins/storefront-order-confirmation/api.js';
 import { render as orderConfirmationRenderer } from '@dropins/storefront-order-confirmation/render.js';
 import { OrderConfirmation } from '@dropins/storefront-order-confirmation/containers/OrderConfirmation.js';
+import { getConfigValue } from '../../scripts/configs.js';
 
 export default async function decorate(block) {
   const commerceEndpoint = await getConfigValue('commerce-core-endpoint');

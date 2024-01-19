af632c0f9f1c5490f1cb4a65dc371f4c7e4e39d4
Mark J. Becker <herzog31@users.noreply.github.com>
2024-01-19 15:43:52 +0100

    Scope PDP styles

--- blocks/commerce-order-confirmation/commerce-order-confirmation.js
+++ blocks/commerce-order-confirmation/commerce-order-confirmation.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import { setEndpoint } from '@dropins/elsie/fetch-graphql.js';
 import { initializers } from '@dropins/elsie/initializer.js';
 import * as orderConfirmationApi from '@dropins/storefront-order-confirmation/api.js';

4c2d388f5cd6eeb80f03f80b2e8e7c6a594158fa
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-06-18 16:03:17 +0530

    be

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,30 +1,55 @@
-const consentBox =  
-    document.getElementById("consentBox"); 
-const acceptBtn =  
-    document.querySelector(".consentButton"); 
-const rejectBtn =  
-    document.querySelector(".rejectButton"); 
+export default async function decorate(block) {
+    console.log("hello from sample block")
+    console.log(block)
+}
+
+const getCookie = (name) => {
+    const value = " " + document.cookie;
+    console.log("value", `==${value}==`);
+    const parts = value.split(" " + name + "=");
+    return parts.length < 2 ? undefined : parts.pop().split(";").shift();
+  };
   
-acceptBtn.onclick = () => { 
-    document.cookie = "CookieBy=GeeksForGeeks; max-age="
-        + 60 * 60 * 24; 
-    if (document.cookie) { 
-        consentBox.classList.add("hide"); 
-    } else { 
-        alert 
-            ("Cookie can't be set! Please"+ 
-              " unblock this site from the cookie"+ 
-              " setting of your browser."); 
-    } 
-}; 
+  const setCookie = function (name, value, expiryDays, domain, path, secure) {
+    const exdate = new Date();
+    exdate.setHours(
+      exdate.getHours() +
+        (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
+    );
+    document.cookie =
+      name +
+      "=" +
+      value +
+      ";expires=" +
+      exdate.toUTCString() +
+      ";path=" +
+      (path || "/") +
+      (domain ? ";domain=" + domain : "") +
+      (secure ? ";secure" : "");
+  };
+  (() => {
+    const $cookiesBanner = document.querySelector(".cookies-eu-banner");
+    const $cookiesBannerButton = $cookiesBanner.querySelector("button");
   
-rejectBtn.onclick = () => { 
-    alert( 
-        "Cookies rejected. Some functionality may be limited."); 
-    consentBox.classList.add("hide"); 
-}; 
-  
-let checkCookie =  
-    document.cookie.indexOf("CookieBy=GeeksForGeeks"); 
-checkCookie !== -1 ? consentBox.classList.add("hide") : 
-    consentBox.classList.remove("hide");
+    $cookiesBannerButton.addEventListener("click", () => {
+      $cookiesBanner.remove();
+    });
+  })();
+  .hidden {
+    display: none;
+  }
+  <div class="cookies-eu-banner hidden">
+...
+const $cookiesBanner = document.querySelector(".cookies-eu-banner");
+const $cookiesBannerButton = $cookiesBanner.querySelector("button");
+const cookieName = "cookiesBanner";
+const hasCookie = getCookie(cookieName);
+
+if (!hasCookie) {
+  $cookiesBanner.classList.remove("hidden");
+}
+
+$cookiesBannerButton.addEventListener("click", () => {
+  setCookie(cookieName, "closed");
+  $cookiesBanner.remove();
+});

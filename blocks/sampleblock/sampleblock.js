166f3df9576f153fcd421e9c3edcb01550a8bd70
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-06-18 15:17:06 +0530

    Update sampleblock.js

---
+++ blocks/sampleblock/sampleblock.js
@@ -0,0 +1,29 @@
+export default async function decorate(block) {
+    console.log("hello from sample block")
+    console.log(block)
+}
+
+const getCookie = (name) => {
+    const value = " " + document.cookie;
+    console.log("value", `==${value}==`);
+    const parts = value.split(" " + name + "=");
+    return parts.length < 2 ? undefined : parts.pop().split(";").shift();
+  };
+  
+  const setCookie = function (name, value, expiryDays, domain, path, secure) {
+    const exdate = new Date();
+    exdate.setHours(
+      exdate.getHours() +
+        (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
+    );
+    document.cookie =
+      name +
+      "=" +
+      value +
+      ";expires=" +
+      exdate.toUTCString() +
+      ";path=" +
+      (path || "/") +
+      (domain ? ";domain=" + domain : "") +
+      (secure ? ";secure" : "");
+  };

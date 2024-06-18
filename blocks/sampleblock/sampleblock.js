a934c68e167f61b2e3f6cda03cc12bfaa940a49e
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-06-18 18:40:06 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -12,22 +12,7 @@ createRejectButton(label) {
     // CREATE BUTTON with class name = ".reject"
 }
 .accept.click() {
-    
-}
-
-.reject.click() {
-    //
-}
-
-const samplebox = 
-	document.getElementById("samplebox"); 
-const acceptBtn = 
-	document.querySelector(".acceptButton"); 
-const rejectBtn = 
-	document.querySelector(".rejectButton"); 
-
-accept.onclick = () => { 
-	document.cookie = "CookieBy=GeeksForGeeks; max-age="
+    document.cookie = "CookieBy=GeeksForGeeks; max-age="
 		+ 60 * 60 * 24; 
 	if (document.cookie) { 
 		consentBox.classList.add("hide"); 
@@ -36,14 +21,15 @@ accept.onclick = () => {
 			("Cookie can't be set! Please"+ 
 			" unblock this site from the cookie"+ 
 			" setting of your browser."); 
-	} 
-}; 
+	}
+}
 
-reject.onclick = () => { 
-	alert( 
+.reject.click() {
+    alert( 
 		"Cookies rejected. Some functionality may be limited."); 
-	consentBox.classList.add("hide"); 
-}; 
+	consentBox.classList.add("hide");
+}
+
 
 let checkCookie = 
 	document.cookie.indexOf("CookieBy=GeeksForGeeks"); 

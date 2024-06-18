9ad682d64563e4fe9a3eeac1bb6496695bbaf628
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-06-18 18:23:50 +0530

    n

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,50 +1,20 @@
-const getCookie = (name) => {
-    const value = " " + document.cookie;
-    console.log("value", `==${value}==`);
-    const parts = value.split(" " + name + "=");
-    return parts.length < 2 ? undefined : parts.pop().split(";").shift();
-  };
-  
-  const setCookie = function (name, value, expiryDays, domain, path, secure) {
-    const exdate = new Date();
-    exdate.setHours(
-      exdate.getHours() +
-        (typeof expiryDays !== "number" ? 365 : expiryDays) * 24
-    );
-    document.cookie =
-      name +
-      "=" +
-      value +
-      ";expires=" +
-      exdate.toUTCString() +
-      ";path=" +
-      (path || "/") +
-      (domain ? ";domain=" + domain : "") +
-      (secure ? ";secure" : "");
-  };
-  (() => {
-    const $cookiesBanner = document.querySelector(".cookies-eu-banner");
-    const $cookiesBannerButton = $cookiesBanner.querySelector("button");
-  
-    $cookiesBannerButton.addEventListener("click", () => {
-      $cookiesBanner.remove();
-    });
-  })();
-  .hidden {
-    display: none;
-  }
-  <div class="cookies-eu-banner hidden">
-...
-const $cookiesBanner = document.querySelector(".cookies-eu-banner");
-const $cookiesBannerButton = $cookiesBanner.querySelector("button");
-const cookieName = "cookiesBanner";
-const hasCookie = getCookie(cookieName);
+export default async function decorate(block) {
+    var acceptLabel = block(data-accept-button-label);
+    var rejectLabel = block(data-reject-button-label);
 
-if (!hasCookie) {
-  $cookiesBanner.classList.remove("hidden");
+    createButton()
 }
 
-$cookiesBannerButton.addEventListener("click", () => {
-  setCookie(cookieName, "closed");
-  $cookiesBanner.remove();
-});
+createPrimaryButton(label) {
+    // CREATE BUTTON with class name = ".accept"
+}
+createRejectButton(label) {
+    // CREATE BUTTON with class name = ".reject"
+}
+.accept.click() {
+    //
+}
+
+.reject.click() {
+    //
+}

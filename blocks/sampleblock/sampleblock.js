0e07721a0f5a8285dd5268b412753a785c011e49
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-06-19 17:31:10 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,19 +1,19 @@
-export default async function decorate(block) {
-    const rows= [...block.children];
-	[...block.children].forEach((row,r) => {
-		if(r==0){
-			const declinebtn = document.createElement('button');
-			declinebtn.classList.decline('btn');
-			declinebtn.classList.decline('btn-decline');
-			const node = documnet.createTextNode(row.textContent;
-				declinebtn.append(node);
-				row.replaceWith(declinebtn);
+// export default async function decorate(block) {
+//     const rows= [...block.children];
+// 	[...block.children].forEach((row,r) => {
+// 		if(r==0){
+// 			const declinebtn = document.createElement('button');
+// 			declinebtn.classList.decline('btn');
+// 			declinebtn.classList.decline('btn-decline');
+// 			const node = documnet.createTextNode(row.textContent;
+// 				declinebtn.append(node);
+// 				row.replaceWith(declinebtn);
 
-			}else if(r==rows.length-1{
-				const acceptbtn = document.createElement('button');
-				prebtn.classList.accept('btn-accept');
-				const node = document.createTextNode(row.textContent);
-				acceptbtnn.append(node;
-					row.replaceWith(acceptbtn);
-		}
-	}
+// 			}else if(r==rows.length-1{
+// 				const acceptbtn = document.createElement('button');
+// 				prebtn.classList.accept('btn-accept');
+// 				const node = document.createTextNode(row.textContent);
+// 				acceptbtnn.append(node;
+// 					row.replaceWith(acceptbtn);
+// 		}
+// 	}

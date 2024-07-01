8f1c75dbb47941240f6bbcd649a2c8377fce9849
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-07-01 18:08:25 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -12,17 +12,10 @@ function convertHeadingToButton(id) {
         button.addEventListener("click", function() {
             alert("Button for " + heading.textContent + " clicked!");
         });
-        
         // Replace the heading with the button
         heading.parentNode.replaceChild(button, heading);
     }
 }
-
-        // Append the button to the button container
-        var buttonContainer = document.getElementById("buttonContainer");
-        if (buttonContainer) {
-            buttonContainer.appendChild(button);
-        }
 
 // List of heading ids to be converted
 var headingIds = ["accept", "decline"];

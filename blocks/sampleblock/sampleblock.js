4097929a3952bac9685edc020786d471628a97dc
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-07-01 14:23:53 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,27 +1,7 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Create a div container for the button
-//     var container = document.createElement("div");
-//     container.id = "button-container";
-
-//     // Create a button element
-//     var button = document.createElement("button");
-//     button.innerHTML = "Accept Cookies";
-
-//     // Add an event listener to handle the click event
-//     button.addEventListener("click", function() {
-//         // Set data-cookieStatus attribute to true
-//         button.setAttribute("data-cookieStatus", "true");
-//         // Add the class "accept" to the button
-//         button.classList.add("accept");
-//     });
-
-//     // Append the button to the container
-//     container.appendChild(button);
-
-//     // Append the container to the body
-//     document.body.appendChild(container);
-// });
-
-export default function decorate(block) {
-    console.log(block)
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Create a div container for the button
+    var container = document.createElement("div");
+    container.id = "button-container";
+    const paragraph = document.querySelector('p');
+    paragraph.id = 'myParagraph';
+    console.log(paragraph);

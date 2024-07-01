0c0f05d571b10fa617437ee0e36c699560792532
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-07-01 16:08:45 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,31 +1,3 @@
-// // Function to convert heading to button
-// function convertHeadingToButton() {
-//     // Get the heading element by its ID
-//     const heading = document.getElementById('accept',);
-
-//     // Create a new button element
-//     const button = document.createElement('button');
-
-//     // Transfer the text content from heading to button
-//     button.textContent = heading.textContent;
-
-//     // Copy attributes from heading to button if necessary
-//     // For example, copy class, style, etc.
-//     button.className = heading.className;
-//     button.style.cssText = heading.style.cssText;
-
-//     // Replace the heading with the button
-//     heading.parentNode.replaceChild(button, heading);
-
-//     // Optional: Add event listener to the button
-//     button.addEventListener('click', function() {
-//         alert('Button clicked!');
-//     });
-// }
-
-// // Call the function to convert the heading to a button
-// convertHeadingToButton();
-
 function convertHeadingToButton(id) {
     // Get the heading element by id
     var heading = document.getElementById(id);
@@ -40,7 +12,7 @@ function convertHeadingToButton(id) {
   }
   
   // List of heading ids to be converted
-  var headingIds = ["accept", "decline",];
+  var headingIds = ["accept", "decline"];
   
   // Loop through the list and convert each heading to a button
   headingIds.forEach(function(id) {

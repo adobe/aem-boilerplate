216db55fc2d12e21efb8c33274ffb83edd5cebde
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-07-01 13:28:34 +0530

    Update sampleblock.js

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -1,23 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Create a div container for the button
-    var container = document.createElement("div");
-    container.id = "button-container";
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Create a div container for the button
+//     var container = document.createElement("div");
+//     container.id = "button-container";
 
-    // Create a button element
-    var button = document.createElement("button");
-    button.innerHTML = "Accept Cookies";
+//     // Create a button element
+//     var button = document.createElement("button");
+//     button.innerHTML = "Accept Cookies";
 
-    // Add an event listener to handle the click event
-    button.addEventListener("click", function() {
-        // Set data-cookieStatus attribute to true
-        button.setAttribute("data-cookieStatus", "true");
-        // Add the class "accept" to the button
-        button.classList.add("accept");
-    });
+//     // Add an event listener to handle the click event
+//     button.addEventListener("click", function() {
+//         // Set data-cookieStatus attribute to true
+//         button.setAttribute("data-cookieStatus", "true");
+//         // Add the class "accept" to the button
+//         button.classList.add("accept");
+//     });
 
-    // Append the button to the container
-    container.appendChild(button);
+//     // Append the button to the container
+//     container.appendChild(button);
 
-    // Append the container to the body
-    document.body.appendChild(container);
-});
+//     // Append the container to the body
+//     document.body.appendChild(container);
+// });
+
+export default function decorate(block) {
+    console.log(block)
+}

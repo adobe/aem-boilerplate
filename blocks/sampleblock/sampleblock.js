e02b9a7bd2b5e1650b8314d3b4e032541ff045a4
Techcapital Software Pvt Ltd <boltposwal@gmail.com>
2024-07-01 16:15:44 +0530

    2

--- blocks/sampleblock/sampleblock.js
+++ blocks/sampleblock/sampleblock.js
@@ -6,6 +6,8 @@ function convertHeadingToButton(id) {
         var button = document.createElement("button");
         // Set the button's text content to the heading's text content
         button.textContent = heading.textContent;
+        // Add a class to the button for styling
+        button.className = "custom-button";
         // Add a click event listener to the button
         button.addEventListener("click", function() {
             alert("Button for " + heading.textContent + " clicked!");
@@ -13,12 +15,12 @@ function convertHeadingToButton(id) {
         // Replace the heading with the button
         heading.parentNode.replaceChild(button, heading);
     }
-  }
-  
-  // List of heading ids to be converted
-  var headingIds = ["accept", "decline"];
-  
-  // Loop through the list and convert each heading to a button
-  headingIds.forEach(function(id) {
+}
+
+// List of heading ids to be converted
+var headingIds = ["accept", "decline"];
+
+// Loop through the list and convert each heading to a button
+headingIds.forEach(function(id) {
     convertHeadingToButton(id);
-  });
+});

4643b51c55016add301167f333c02b471b5709fb
Dirk Rudolph <drudolph@adobe.com>
2024-07-31 09:47:02 +0200

    feat: fragment block for crosswalk (#36)
    * feat: add fragment def and model
    
    * feat: add fragment def and model
    
    * feat: preserve the fragment block, replace contents with fragment

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -19,6 +19,8 @@ import {
  */
 export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
+    // eslint-disable-next-line no-param-reassign
+    path = path.replace(/(\.plain)?\.html/, '');
     const resp = await fetch(`${path}.plain.html`);
     if (resp.ok) {
       const main = document.createElement('main');
@@ -48,8 +50,9 @@ export default async function decorate(block) {
   if (fragment) {
     const fragmentSection = fragment.querySelector(':scope .section');
     if (fragmentSection) {
-      block.closest('.section').classList.add(...fragmentSection.classList);
-      block.closest('.fragment').replaceWith(...fragment.childNodes);
+      block.classList.add(...fragmentSection.classList);
+      block.classList.remove('section');
+      block.replaceChildren(...fragmentSection.childNodes);
     }
   }
 }

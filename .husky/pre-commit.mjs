b59c4012df8b364d7fd118c57c56a624acd9ba58
Joseph Rignanese <joseph.rignanese@gmail.com>
2024-09-18 14:51:53 +1000

    fix: refined list of files added by pre-commit script (#44)

--- .husky/pre-commit.mjs
+++ .husky/pre-commit.mjs
@@ -16,5 +16,5 @@ const modifledPartials = modifiedFiles.filter((file) => file.match(/(^|\/)_.*.js
 if (modifledPartials.length > 0) {
   const output = await run('npm run build:json --silent');
   console.log(output);
-  await run('git add .');
+  await run('git add component-models.json component-definition.json component-filters.json');
 }

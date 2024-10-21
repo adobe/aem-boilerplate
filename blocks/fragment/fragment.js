1690e4063ccb198de7829d39d4397f77901278d5
Andrei Tuicu <tuicu@adobe.com>
2024-10-21 16:49:10 +0200

    feat: Dynamically Import DOMPurify

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -3,7 +3,7 @@
  * Include content on a page as a fragment.
  * https://www.aem.live/developer/block-collection/fragment
  */
-
+import '../../scripts/purify.min.js';
 import {
   decorateMain,
 } from '../../scripts/scripts.js';

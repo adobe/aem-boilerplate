1fd2be0b2c5e23d69bce5c75a8c0e393d97a3236
Stephen <sirugh@users.noreply.github.com>
2024-11-07 10:34:00 -0600

    only initialize event collection when not prerendering (#168)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,39 +3,49 @@ import { getConfigValue } from './configs.js';
 import { getUserTokenCookie } from './initializers/index.js';
 import { getConsent } from './scripts.js';
 
-// Load Commerce events SDK and collector
-if (getConsent('commerce-collection')) {
-  const config = {
-    environmentId: await getConfigValue('commerce-environment-id'),
-    environment: await getConfigValue('commerce-environment'),
-    storeUrl: await getConfigValue('commerce-store-url'),
-    websiteId: parseInt(await getConfigValue('commerce-website-id'), 10),
-    websiteCode: await getConfigValue('commerce-website-code'),
-    storeId: parseInt(await getConfigValue('commerce-store-id'), 10),
-    storeCode: await getConfigValue('commerce-store-code'),
-    storeViewId: parseInt(await getConfigValue('commerce-store-view-id'), 10),
-    storeViewCode: await getConfigValue('commerce-store-view-code'),
-    websiteName: await getConfigValue('commerce-website-name'),
-    storeName: await getConfigValue('commerce-store-name'),
-    storeViewName: await getConfigValue('commerce-store-view-name'),
-    baseCurrencyCode: await getConfigValue('commerce-base-currency-code'),
-    storeViewCurrencyCode: await getConfigValue('commerce-base-currency-code'),
-    storefrontTemplate: 'EDS',
-  };
+async function initAnalytics() {
+  // Load Commerce events SDK and collector
+  if (getConsent('commerce-collection')) {
+    const config = {
+      environmentId: await getConfigValue('commerce-environment-id'),
+      environment: await getConfigValue('commerce-environment'),
+      storeUrl: await getConfigValue('commerce-store-url'),
+      websiteId: parseInt(await getConfigValue('commerce-website-id'), 10),
+      websiteCode: await getConfigValue('commerce-website-code'),
+      storeId: parseInt(await getConfigValue('commerce-store-id'), 10),
+      storeCode: await getConfigValue('commerce-store-code'),
+      storeViewId: parseInt(await getConfigValue('commerce-store-view-id'), 10),
+      storeViewCode: await getConfigValue('commerce-store-view-code'),
+      websiteName: await getConfigValue('commerce-website-name'),
+      storeName: await getConfigValue('commerce-store-name'),
+      storeViewName: await getConfigValue('commerce-store-view-name'),
+      baseCurrencyCode: await getConfigValue('commerce-base-currency-code'),
+      storeViewCurrencyCode: await getConfigValue('commerce-base-currency-code'),
+      storefrontTemplate: 'EDS',
+    };
 
-  window.adobeDataLayer.push(
-    { storefrontInstanceContext: config },
-    { eventForwardingContext: { commerce: true, aep: false } },
-    {
-      shopperContext: {
-        shopperId: getUserTokenCookie() ? 'logged-in' : 'guest',
+    window.adobeDataLayer.push(
+      { storefrontInstanceContext: config },
+      { eventForwardingContext: { commerce: true, aep: false } },
+      {
+        shopperContext: {
+          shopperId: getUserTokenCookie() ? 'logged-in' : 'guest',
+        },
       },
-    },
-  );
+    );
 
-  // Load events SDK and collector
-  import('./commerce-events-sdk.js');
-  import('./commerce-events-collector.js');
+    // Load events SDK and collector
+    import('./commerce-events-sdk.js');
+    import('./commerce-events-collector.js');
+  }
+}
+
+if (document.prerendering) {
+  document.addEventListener('prerenderingchange', initAnalytics, {
+    once: true,
+  });
+} else {
+  initAnalytics();
 }
 
 // add delayed functionality here

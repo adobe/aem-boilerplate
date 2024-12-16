6b3b38f3cfa301316b57c4c2f2f64ac017f25feb
Chris Millar <cmillar@apache.org>
2024-12-16 09:09:14 -0700

    DA Live Preview (#256)
    * DA Live Preview
    
    Add live preview support for Document Authoring for customers who use the new Commerce Scaffold CLI.
    
    ## Note:
    If there is no query param, the script will not be imported. Either way, this runs after loadPage and does not impact performance.
    
    Before: https://main--aem-boilerplate-commerce--hlxsites.hlx.page/
    
    After: https://da-live-preview--aem-boilerplate-commerce--hlxsites.hlx.page/
    
    * Update 404.html
    
    Remove use credentials.
    
    1. This property is not necessary.
    2. This property breaks da live preview.
    
    ---------
    
    Co-authored-by: Stephen <sirugh@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -374,3 +374,9 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  // eslint-disable-next-line import/no-unresolved
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());

8b8ece54e2e72c9322427386404fe671b15848e8
Anthoula Wojczak <anthoula@users.noreply.github.com>
2025-02-04 13:33:18 -0600

    update packages to 0.40.0 (#296)

--- scripts/__dropins__/tools/types/elsie/src/components/Tag/Tag.d.ts
+++ scripts/__dropins__/tools/types/elsie/src/components/Tag/Tag.d.ts
@@ -1,8 +1,9 @@
-import { FunctionComponent } from 'preact';
+import { FunctionComponent, VNode } from 'preact';
 import { HTMLAttributes } from 'preact/compat';
 
 export interface TagProps extends Omit<HTMLAttributes<HTMLDivElement>, 'onChange' | 'icon'> {
-    label: string;
+    label?: string;
+    children?: VNode | VNode[];
 }
 export declare const Tag: FunctionComponent<TagProps>;
 //# sourceMappingURL=Tag.d.ts.map

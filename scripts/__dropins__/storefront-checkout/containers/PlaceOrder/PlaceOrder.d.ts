b8813cf0f2f2577ece789c3f1c48ad5767679ac2
Mike <mcocachan@gmail.com>
2025-02-11 15:27:50 +0100

    New Checkout version v1.2.0 (#313)

--- scripts/__dropins__/storefront-checkout/containers/PlaceOrder/PlaceOrder.d.ts
+++ scripts/__dropins__/storefront-checkout/containers/PlaceOrder/PlaceOrder.d.ts
@@ -1,13 +1,20 @@
+import { SlotProps } from '@dropins/tools/types/elsie/src/lib';
 import { HTMLAttributes } from 'preact/compat';
 
+export interface ContentSlotContext {
+    code: string;
+}
+export interface HandlePlaceOrderContext {
+    code: string;
+    cartId: string;
+}
 export interface PlaceOrderProps extends HTMLAttributes<HTMLDivElement> {
     disabled?: boolean;
     handleValidation?: () => boolean;
-    handlePlaceOrder: (ctx: PlaceOrderContext) => Promise<void>;
-}
-export interface PlaceOrderContext {
-    code: string;
-    cartId: string;
+    handlePlaceOrder: (ctx: HandlePlaceOrderContext) => Promise<void>;
+    slots?: {
+        Content?: SlotProps<ContentSlotContext>;
+    };
 }
 export declare const PlaceOrder: {
     ({ hideOnEmptyCart, hideOnVirtualCart, ...props }: import('../../hocs/withConditionalRendering').ConditionalProps & PlaceOrderProps): import("preact/compat").JSX.Element;

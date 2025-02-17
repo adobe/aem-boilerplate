783a7a4166afd587b3f1885d397834ca92b30f55
Adarsh Nanda <adarshn@adobe.com>
2025-02-17 16:09:12 +0530

    slots update

--- blocks/lnb-container/lnb-container.js
+++ blocks/lnb-container/lnb-container.js
@@ -1,11 +1,11 @@
-
-function createSection(selctor, name){
-    const childDiv = document.createElement('div');
-    const slot = document.querySelector(`.${selctor}`);
+function createSection(selctor, name) {
+  const childDiv = document.createElement("div");
+  const slots = document.querySelectorAll(`.${selctor}`);
+  [...slots].forEach((slot) => {
     childDiv.append(slot);
-    childDiv.classList.add(name);
-    return childDiv;
-
+  });
+  childDiv.classList.add(name);
+  return childDiv;
 }
 export default function decorate(block) {
   /* change to ul, li */
@@ -14,7 +14,7 @@ export default function decorate(block) {
   const [child] = block.children;
   const [left, right] = [...child.children];
 
-  const leftSection = createSection(left.innerText, 'lnb');
+  const leftSection = createSection(left.innerText, "lnb");
   const rightSection = createSection(right.innerText, "page-box");
   div.append(leftSection);
   div.append(rightSection);

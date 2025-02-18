5e0b38880f333ae6b0bb89930111939a85681d65
Anthoula Wojczak <anthoula@users.noreply.github.com>
2025-02-18 13:48:23 -0600

    update packages to 0.41.0 (#327)

--- scripts/__dropins__/tools/types/elsie/src/components/Checkbox/Checkbox.d.ts
+++ scripts/__dropins__/tools/types/elsie/src/components/Checkbox/Checkbox.d.ts
@@ -1,13 +1,13 @@
 import { FunctionComponent, VNode } from 'preact';
 import { HTMLAttributes } from 'preact/compat';
 
-export interface CheckboxProps extends Omit<HTMLAttributes<HTMLInputElement>, 'size'> {
+export interface CheckboxProps extends Omit<HTMLAttributes<HTMLInputElement>, 'size' | 'label'> {
     name: string;
     size?: 'medium' | 'large';
     disabled?: boolean;
     error?: boolean;
-    label?: string | VNode[];
-    description?: string | VNode[];
+    label?: string | VNode | VNode[];
+    description?: string | VNode | VNode[];
 }
 export declare const Checkbox: FunctionComponent<CheckboxProps>;
 //# sourceMappingURL=Checkbox.d.ts.map

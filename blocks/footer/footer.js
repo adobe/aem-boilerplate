e8e574f8f8994219f4b00d1563adad1a590cf961
Adarsh Nanda <adarshn@adobe.com>
2025-02-18 12:28:21 +0530

    footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,60 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
+
+function createLinks(links) {
+  const linksElem = links.map((link) => {
+    const href = link.querySelector("a")?.href || '#';
+    return `<li class="snbs__item">
+        <a
+          href="${href}"
+          class="snbs__link"
+          target="_self"
+        >
+          ${link.innerText}
+        </a>`;
+  });
+  return linksElem.join("");
+}
+
+
+function createSnbGroup(gropus) {
+  const gropupElem = gropus.map((group, index)=>{
+    const title = group.firstChild.innerText;
+    const linksElem = group.querySelectorAll("li");
+    const links = createLinks([...linksElem])
+    return `<li class="snbs__group" data-role="item">
+  <h6 class="snbs__title _mo-hidden" id="snb_heading${index}">
+          ${title}
+  </h6>
+  <h6 class="snbs__heading _pc-hidden">
+    <button
+      name="0"
+      class="snbs__button _accordion-btn"
+      type="button"
+      aria-expanded="false"
+      aria-controls="snbs_panel${index}"
+      data-role="title"
+    >
+      ${title}
+    </button>
+  </h6>
+  <div
+    role="region"
+    aria-labelledby="snb_heading0"
+    id="snbs_panel${index}"
+    data-role="panel"
+  >
+    <ul id="snbs0" class="snbs__list">
+        ${links}
+      </li>
+    </ul>
+  </div>
+</li>`;
+  })
+return gropupElem.join('');
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -14,7 +68,26 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  footer.classList.add("shortcuts");
+
+  const container = footer.querySelector(".shortcuts__box");
+
+  if(!fragment.firstElementChild){
+    return;
+  }
+  const gropus = fragment.firstElementChild.querySelectorAll(".default-content-wrapper>ul>li");
+  const grpElems = createSnbGroup([...gropus]);
+
+  footer.innerHTML = `
+  <div class="shortcuts__aligner">
+    <div class="shortcuts__box">
+      <div class="shortcuts__snbs">
+        <ul class="snbs accordion">
+          ${grpElems}
+        <ul>
+      <div>
+    </div>
+  </div>`;
 
   block.append(footer);
 }

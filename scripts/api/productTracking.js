2d8c5c1f208a69696d21ee6a7e2a7f87d4b59e6b
eliseacornejo <ecornejo@adobe.com>
2025-02-20 14:23:36 +0100

    Change the track product view mutation call to other place (#326)

--- scripts/api/productTracking.js
+++ scripts/api/productTracking.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 import { getSignInToken, performMonolithGraphQLQuery } from '../commerce.js';
 
 const trackViewedProductMutation = `
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,6 +2,7 @@
 import { getConfigValue } from './configs.js';
 import { getUserTokenCookie } from './initializers/index.js';
 import { getConsent } from './scripts.js';
+import trackViewedProduct from './api/productTracking.js';
 
 async function initAnalytics() {
   // Load Commerce events SDK and collector
@@ -33,6 +34,10 @@ async function initAnalytics() {
         },
       },
     );
+
+    window.adobeDataLayer.push((dl) => {
+      dl.addEventListener('product-page-view', ({ eventInfo }) => trackViewedProduct(eventInfo.productContext.sku));
+    });
 
     // Load events SDK and collector
     import('./commerce-events-sdk.js');

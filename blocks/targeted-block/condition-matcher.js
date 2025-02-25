f1d5eea1001e0c8c5cb0cf6be375eba29c2f1490
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2025-02-25 15:55:02 +0000

    LYNX-756: Google UTM parameters support (#339)

--- blocks/targeted-block/condition-matcher.js
+++ blocks/targeted-block/condition-matcher.js
@@ -12,29 +12,41 @@ export default function conditionsMatched(activeRules, blockConfig) {
     'catalog-price-rules': catalogPriceRules,
   } = blockConfig;
 
-  const activeSegments = activeRules.customerSegments?.map(
-    (segment) => segment.name.trim(),
-  );
-  const activeGroup = activeRules.customerGroup?.name.trim();
-  const activeCartRules = activeRules.cart?.map(
-    (rule) => rule.name.trim(),
-  );
-  const activePriceRules = activeRules.catalogPriceRules?.rules?.map(
-    (rule) => rule.name.trim(),
+  const currentUrlParams = new URLSearchParams(window.location.search);
+  const utmParamsConfig = Object.keys(blockConfig).filter(
+    (key) => (key.indexOf('utm-') === 0),
+  ).reduce(
+    (data, key) => {
+      data[key.replace('-', '_')] = blockConfig[key];
+      return data;
+    },
+    {},
   );
 
+  const nonMatchingUtmParams = Object.keys(utmParamsConfig).filter(
+    (key) => (currentUrlParams.get(key.replace('-', '_')) !== utmParamsConfig[key]),
+  );
+
+  if (nonMatchingUtmParams.length !== 0) {
+    return false;
+  }
+
+  const activeSegments = activeRules.customerSegments?.map((segment) => segment.name.trim());
   if (customerSegments !== undefined && !rulesMatched(activeSegments, customerSegments.split(','))) {
     return false;
   }
 
+  const activeGroup = activeRules.customerGroup?.name.trim();
   if (customerGroups !== undefined && !groupMatched(activeGroup, customerGroups.split(','))) {
     return false;
   }
 
+  const activeCartRules = activeRules.cart?.map((rule) => rule.name.trim());
   if (cartRules !== undefined && !rulesMatched(activeCartRules, cartRules.split(','))) {
     return false;
   }
 
+  const activePriceRules = activeRules.catalogPriceRules?.rules?.map((rule) => rule.name.trim());
   if (catalogPriceRules !== undefined && !rulesMatched(activePriceRules, catalogPriceRules.split(','))) {
     return false;
   }

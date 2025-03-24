10215451a7b1e37800fc22827311e73a4a0c30f2
Laurel Timko <laurel.timko@jmp.com>
2025-03-24 13:04:25 -0400

    Change filter dropdowns to index properties

--- tools/querybuilder/querybuilder.js
+++ tools/querybuilder/querybuilder.js
@@ -2,7 +2,6 @@
 // eslint-disable-next-line import/no-unresolved
 // import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 import {
-  createTag,
   getQueryIndex,
 } from '../../scripts/utilities.js';
 
@@ -12,70 +11,32 @@ export async function getProperties() {
   return properties;
 }
 
-export function addForm() {
+async function buildFilters() {
+  const properties = await getProperties();
+  const filters = [];
+  for (let i=0; i < properties.length; i++) {
+    const prop = properties[i];
+    console.log(prop);
+    const propFilter = {
+      id: `${prop}`,
+      label: `${prop}`,
+      type: 'string',
+      operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null'],
+    };
+    filters.push(propFilter);
+  }
+  return filters;
+}
+
+
+export function addForm(searchFilters) {
   $('#builder-basic').queryBuilder({
     plugins: ['bt-tooltip-errors'],
-
-    filters: [{
-      id: 'name',
-      label: 'Name',
-      type: 'string',
-    }, {
-      id: 'category',
-      label: 'Category',
-      type: 'integer',
-      input: 'select',
-      values: {
-        1: 'Books',
-        2: 'Movies',
-        3: 'Music',
-        4: 'Tools',
-        5: 'Goodies',
-        6: 'Clothes',
-      },
-      operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null'],
-    }, {
-      id: 'in_stock',
-      label: 'In stock',
-      type: 'integer',
-      input: 'radio',
-      values: {
-        1: 'Yes',
-        0: 'No',
-      },
-      operators: ['equal'],
-    }, {
-      id: 'price',
-      label: 'Price',
-      type: 'double',
-      validation: {
-        min: 0,
-        step: 0.01,
-      },
-    }, {
-      id: 'id',
-      label: 'Identifier',
-      type: 'string',
-      placeholder: '____-____-____',
-      operators: ['equal', 'not_equal'],
-      validation: {
-        format: /^.{4}-.{4}-.{4}$/,
-      },
-    }],
+    filters: searchFilters,
   });
 }
 
-async function buildDropdownOptions() {
-  const propertyDropdown = document.querySelector('form#propertyCheck select#propertyName');
-  const properties = await getProperties();
-  console.log(properties);
-  properties.forEach((prop) => {
-    const option = createTag('option', { value: `${prop}` }, `${prop}`);
-    propertyDropdown.append(option);
-  });
-}
-
-function initControls() {
+function initControls(actions) {
   $('#btn-reset').on('click', (e) => {
     e.preventDefault();
     $('#builder-basic').queryBuilder('reset');
@@ -87,15 +48,16 @@ function initControls() {
     console.log(result);
 
     if (!$.isEmptyObject(result)) {
-      alert(JSON.stringify(result, null, 2));
+      actions.sendText(JSON.stringify(result, null, 2));
+      actions.closeLibrary();
     }
   });
 }
 
 (async function init() {
-  // const { actions } = await DA_SDK;
+  const { actions } = await DA_SDK;
 
-  buildDropdownOptions();
-  addForm();
-  initControls();
+  const searchFilters = await buildFilters();
+  addForm(searchFilters);
+  initControls(actions);
 }());

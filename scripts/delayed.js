91ff79f4ee557b54ae2b255db61228f1280f5d5a
Carlos A. Cabrera <316104+fnhipster@users.noreply.github.com>
2025-04-08 14:34:34 -0500

    feature: Split storefront config and analytics config (#372)
    * refactor: replace config value retrieval with GraphQL fetch for analytics initialization
    
    * refactor: streamline analytics initialization with improved error handling and GraphQL data fetching
    
    - Updated the GraphQL query to fetch store configuration data and renamed the returned object for clarity.
    - Enhanced error handling during the analytics initialization process.
    - Simplified the configuration object by spreading the fetched data.
    
    * refactor: enhance analytics initialization by replacing GraphQL fetch with direct JSON configuration retrieval
    
    - Removed GraphQL dependency and replaced it with a fetch call to retrieve analytics configuration from a JSON file.
    - Simplified the configuration object creation by utilizing a helper function to extract values from the fetched data.
    - Improved the handling of environment and store-related values for better clarity and maintainability.
    
    * update session storage configuration for Cypress tests

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,49 +1,62 @@
 /* eslint-disable import/no-cycle */
 import { getConfigValue } from './configs.js';
 import { getUserTokenCookie } from './initializers/index.js';
-import { getConsent } from './scripts.js';
+import { getConsent, getRootPath } from './scripts.js';
 
 async function initAnalytics() {
-  // Load Commerce events SDK and collector
-  if (getConsent('commerce-collection')) {
-    const config = {
-      environmentId: await getConfigValue('commerce.headers.cs.Magento-Environment-Id'),
-      environment: await getConfigValue('commerce-environment'),
-      storeUrl: await getConfigValue('commerce-store-url'),
-      websiteId: parseInt(await getConfigValue('commerce-website-id'), 10),
-      websiteCode: await getConfigValue('commerce.headers.cs.Magento-Website-Code'),
-      storeId: parseInt(await getConfigValue('commerce-store-id'), 10),
-      storeCode: await getConfigValue('commerce.headers.cs.Magento-Store-Code'),
-      storeViewId: parseInt(await getConfigValue('commerce-store-view-id'), 10),
-      storeViewCode: await getConfigValue('commerce.headers.cs.Magento-Store-View-Code'),
-      websiteName: await getConfigValue('commerce-website-name'),
-      storeName: await getConfigValue('commerce-store-name'),
-      storeViewName: await getConfigValue('commerce-store-view-name'),
-      baseCurrencyCode: await getConfigValue('commerce-base-currency-code'),
-      storeViewCurrencyCode: await getConfigValue('commerce-base-currency-code'),
-      storefrontTemplate: 'EDS',
-    };
+  try {
+    // Load Commerce events SDK and collector
+    if (getConsent('commerce-collection')) {
+      const root = getRootPath();
 
-    window.adobeDataLayer.push(
-      { storefrontInstanceContext: config },
-      { eventForwardingContext: { commerce: true, aep: false } },
-      {
-        shopperContext: {
-          shopperId: getUserTokenCookie() ? 'logged-in' : 'guest',
+      const config = await fetch(`${root}analytics-config.json`, { cache: 'force-cache' })
+        .then((res) => res.json())
+        .then(async ({ data }) => {
+          const getValue = (key) => {
+            const value = data.find((item) => item.key === key)?.value;
+            return value;
+          };
+
+          return {
+            environmentId: await getConfigValue('commerce.headers.cs.Magento-Environment-Id'),
+            environment: getValue('commerce-environment'),
+            storeUrl: getValue('commerce-store-url'),
+            websiteId: parseInt(getValue('commerce-website-id'), 10),
+            websiteCode: await getConfigValue('commerce.headers.cs.Magento-Website-Code'),
+            storeId: parseInt(getValue('commerce-store-id'), 10),
+            storeCode: await getConfigValue('commerce.headers.cs.Magento-Store-Code'),
+            storeViewId: parseInt(getValue('commerce-store-view-id'), 10),
+            storeViewCode: await getConfigValue('commerce.headers.cs.Magento-Store-View-Code'),
+            websiteName: getValue('commerce-website-name'),
+            storeName: getValue('commerce-store-name'),
+            storeViewName: getValue('commerce-store-view-name'),
+            baseCurrencyCode: getValue('commerce-base-currency-code'),
+            storeViewCurrencyCode: getValue('commerce-base-currency-code'),
+            storefrontTemplate: 'EDS',
+          };
+        });
+
+      window.adobeDataLayer.push(
+        { storefrontInstanceContext: config },
+        { eventForwardingContext: { commerce: true, aep: false } },
+        {
+          shopperContext: {
+            shopperId: getUserTokenCookie() ? 'logged-in' : 'guest',
+          },
         },
-      },
-    );
+      );
 
-    // Load events SDK and collector
-    import('./commerce-events-sdk.js');
-    import('./commerce-events-collector.js');
+      // Load events SDK and collector
+      import('./commerce-events-sdk.js');
+      import('./commerce-events-collector.js');
+    }
+  } catch (error) {
+    console.warn('Error initializing analytics', error);
   }
 }
 
 if (document.prerendering) {
-  document.addEventListener('prerenderingchange', initAnalytics, {
-    once: true,
-  });
+  document.addEventListener('prerenderingchange', initAnalytics, { once: true });
 } else {
   initAnalytics();
 }

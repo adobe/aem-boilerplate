8a7fa458af372783c54f724e1be7615ffa37763f
Daniela Neagu <dana.neagu1@gmail.com>
2025-04-11 08:49:57 +0300

    [PDP] enable graphql extensibility for PDP drop-in (#364)
    * enable graphql extensibility for pdp drop-in
    
    * Update storefront-pdp to 1.1.0-alpha2 and enhance low stock display logic
    
    * update PDP drop-in to v1.1.0
    
    * update build files
    
    * fix lint
    
    * comment code needed to opt-in on GraphQL extensibility
    
    * remove code needed for testing only
    
    * update build files
    
    ---------
    
    Co-authored-by: Carlos A. Cabrera <cacabrer@adobe.com>
    Co-authored-by: Dev Patil <40405790+devpatil7@users.noreply.github.com>

---
+++ scripts/__dropins__/storefront-pdp/api/graphql/PriceRangeFragment.graphql.d.ts
@@ -0,0 +1,17 @@
+/**
+ * ADOBE CONFIDENTIAL
+ * __________________
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ * __________________
+ * NOTICE: All information contained herein is, and remains
+ * the property of Adobe and its suppliers, if any. The intellectual
+ * and technical concepts contained herein are proprietary to Adobe
+ * and its suppliers and are protected by all applicable intellectual
+ * property laws, including trade secret and copyright laws.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe.
+ */
+export declare const PRICE_RANGE_FRAGMENT = "\n  fragment PRICE_RANGE_FRAGMENT on ComplexProductView {\n    priceRange {\n      maximum {\n        final {\n          amount {\n            value\n            currency\n          }\n        }\n        regular {\n          amount {\n            value\n            currency\n          }\n        }\n        roles\n      }\n      minimum {\n        final {\n          amount {\n            value\n            currency\n          }\n        }\n        regular {\n          amount {\n            value\n            currency\n          }\n        }\n        roles\n      }\n    }\n  }\n";
+//# sourceMappingURL=PriceRangeFragment.graphql.d.ts.map

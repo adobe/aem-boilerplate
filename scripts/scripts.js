2ef881e713144c77433ac4176a3e05dbec68e094
Tad Reeves <tad@arborydigital.com>
2025-04-17 10:27:43 -0700

    Fix for live preview

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -346,3 +346,10 @@ async function loadPage() {
 }
 
 loadPage();
+
+// DA Live Preview
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  // eslint-disable-next-line import/no-unresolved
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());

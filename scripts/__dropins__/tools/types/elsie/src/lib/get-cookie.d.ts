f46e69a112b8097a6b6e92fb02a4b1f0bf7cce73
Chuting (Christina) Yan <59424839+cctina516@users.noreply.github.com>
2025-06-11 11:44:55 -0500

    update sdk to betaone (#508)

---
+++ scripts/__dropins__/tools/types/elsie/src/lib/get-cookie.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Get cookie
+ * @param {string} cookieName - The name of the cookie to get
+ * @returns {string} - The value of the cookie
+ */
+export declare function getCookie(cookieName: string): string | undefined;
+//# sourceMappingURL=get-cookie.d.ts.map
---
+++ scripts/__dropins__/tools/types/elsie/src/lib/get-path-value.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Retrieves a value from a config object using dot notation.
+ *
+ * @param {Object} obj - The config object.
+ * @param {string} key - The key to retrieve (supports dot notation).
+ * @returns {any} - The value of the key.
+ */
+export declare function getPathValue(obj: Record<string, any>, key: string): any;
+//# sourceMappingURL=get-path-value.d.ts.map

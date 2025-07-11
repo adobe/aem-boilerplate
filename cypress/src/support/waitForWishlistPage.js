801c80aeb74f46fad490bf9c100808ae975b6493
eliseacornejo <ecornejo@adobe.com>
2025-07-11 15:51:35 +0200

    USF-2464 (#609)

--- cypress/src/support/waitForWishlistPage.js
+++ cypress/src/support/waitForWishlistPage.js
@@ -3,12 +3,20 @@
  * This command ensures the page is ready before running assertions
  */
 Cypress.Commands.add('waitForWishlistPageLoaded', () => {
-  // Wait for loading to disappear and page to be fully loaded
+  cy.url().should('include', '/wishlist')
+  // Wait for the wishlist wrapper to exist
+  cy.get('.commerce-wishlist-wrapper').should("exist");
+
+  // Wait for generic loading to disappear
   cy.get('body').should('not.contain', 'Loading...');
-  
-  // Ensure we're on the wishlist page
-  cy.url().should('include', '/wishlist');
-  
-  // Wait for the main wishlist content area to be present
-  cy.get('.commerce-wishlist-wrapper').should('be.visible');
+
+  // Wait for actual content to appear (either wishlist items or empty state)
+  cy.get('body').should($body => {
+    const hasWishlistHeading = $body.find('[data-testid="default-wishlist-heading"]').length > 0;
+    const hasEmptyWishlist = $body.find('[data-testid="empty-wishlist"]').length > 0;
+    const hasWishlistItems = $body.find('.wishlist-product-item').length > 0;
+
+    // At least one of these should be present when loading is complete
+    expect(hasWishlistHeading || hasEmptyWishlist || hasWishlistItems).to.be.true;
+  });
 }); 

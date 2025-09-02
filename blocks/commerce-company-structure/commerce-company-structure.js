8eb9fc22facf17b468df2d53192d5c9a3c4b82e5
Ji Lu <jilu1@adobe.com>
2025-09-02 11:29:20 -0500

    USF-2696: Added Company Structure side menu and prepared block for rendering Company Structure dropin

--- blocks/commerce-company-structure/commerce-company-structure.js
+++ blocks/commerce-company-structure/commerce-company-structure.js
@@ -1,10 +1,10 @@
-import { render as provider } from '../../scripts/__dropins__/storefront-company-management/render.js';
-import { CompanyStructure } from '../../scripts/__dropins__/storefront-company-management/containers/CompanyStructure.js';
+import { render as provider } from '@dropins/storefront-company-management/render.js';
+import { CompanyStructure } from '@dropins/storefront-company-management/containers/CompanyStructure.js';
 import { checkIsAuthenticated, rootLink } from '../../scripts/commerce.js';
 import '../../scripts/initializers/company.js';
 
 export default async function decorate(block) {
-  block.classList.add('commerce-b2b-company-structure-container');
+  block.classList.add('commerce-company-structure-container');
 
   const isAuthenticated = checkIsAuthenticated();
 
--- scripts/initializers/company.js
+++ scripts/initializers/company.js
@@ -1,18 +1,18 @@
 import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializers } from '@dropins/tools/initializer.js';
-import { initialize, setFetchGraphQlHeaders } from '../__dropins__/storefront-company-management/api.js';
+import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-company-management/api.js';
+import { initializeDropin } from './index.js';
 import { fetchPlaceholders } from '../commerce.js';
 
-await (async () => {
+await initializeDropin(async () => {
   setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('company') }));
 
-  let langDefinitions;
-  try {
-    const labels = await fetchPlaceholders('placeholders/company.json');
-    langDefinitions = { default: { ...labels } };
-  } catch {
-    langDefinitions = undefined;
-  }
+  const labels = await fetchPlaceholders('placeholders/company.json');
+  const langDefinitions = {
+    default: {
+      ...labels,
+    },
+  };
 
   return initializers.mountImmediately(initialize, { langDefinitions });
-})();
+});

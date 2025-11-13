04f2eb5325ab1706e56996308e61b304ae1ae338
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2025-11-13 15:19:18 +0000

    USF-3377: Integrate Universal Editor to boilerplate (#891)

--- .husky/pre-commit.mjs
+++ .husky/pre-commit.mjs
@@ -13,8 +13,8 @@ const changeset = await run('git diff --cached --name-only --diff-filter=ACMR');
 const modifiedFiles = changeset.split('\n').filter(Boolean);
 
 // check if there are any model files staged
-const modifledPartials = modifiedFiles.filter((file) => file.match(/(^|\/)_.*.json/));
-if (modifledPartials.length > 0) {
+const modifiedPartials = modifiedFiles.filter((file) => file.match(/(^|\/)_.*.json/));
+if (modifiedPartials.length > 0) {
     const output = await run('npm run build:json --silent');
     console.log(output);
     await run('git add component-models.json component-definition.json component-filters.json');

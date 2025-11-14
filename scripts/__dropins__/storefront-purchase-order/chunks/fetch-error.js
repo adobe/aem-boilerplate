cd8135a789db1e9e0dfbb1cc8cfe721d16e172ac
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2025-11-14 18:20:25 +0100

    [PO] Update PO to beta version (#900)
    * Bump PO to the latest version
    
    * Adjust version formatting
    
    * Fix broken route
    
    * Install latest PO version

--- scripts/__dropins__/storefront-purchase-order/chunks/fetch-error.js
+++ scripts/__dropins__/storefront-purchase-order/chunks/fetch-error.js
@@ -1,4 +1,4 @@
 /*! Copyright 2025 Adobe
 All Rights Reserved. */
-const s=t=>{var e,n;if(t.length===1&&((n=(e=t[0])==null?void 0:e.path)==null?void 0:n.length)>0){const a=t[0].path[t[0].path.length-1];if(["applied_coupons","applied_gift_cards"].includes(a))return}const o=t.map(a=>a.message).join(" ");throw Error(o)};export{s as h};
+const r=t=>{var e,l;if(t.length===1&&((l=(e=t[0])==null?void 0:e.path)==null?void 0:l.length)>0){const a=t[0].path[t[0].path.length-1];if(["applied_coupons","applied_gift_cards","available_payment_methods"].includes(a))return}const n=t.map(a=>a.message).join(" ");throw Error(n)};export{r as h};
 //# sourceMappingURL=fetch-error.js.map

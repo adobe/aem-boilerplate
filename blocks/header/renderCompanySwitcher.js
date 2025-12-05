58a6f3fd1a2ca0ca80cb1e28d82c18da0c37453f
Carlos A. Cabrera <cacabrer@adobe.com>
2025-12-05 16:29:23 -0600

    Enhance company switcher by implementing a redirection mechanism for order details and negotiable quote templates. Update handleCompanyChange function to manage URL redirections, improving user navigation during company changes.

--- blocks/header/renderCompanySwitcher.js
+++ blocks/header/renderCompanySwitcher.js
@@ -1,6 +1,19 @@
 import { render } from '@dropins/storefront-company-switcher/render.js';
 import { CompanySwitcher } from '@dropins/storefront-company-switcher/containers/CompanySwitcher.js';
 import '../../scripts/initializers/company-switcher.js';
+import {
+  CUSTOMER_NEGOTIABLE_QUOTE_PATH,
+  CUSTOMER_NEGOTIABLE_QUOTE_TEMPLATE_PATH,
+  CUSTOMER_ORDER_DETAILS_PATH,
+  rootLink,
+} from '../../scripts/commerce.js';
+
+// Redirection rules:
+const redirections = {
+  '/customer/order-details?orderRef=': rootLink(CUSTOMER_ORDER_DETAILS_PATH),
+  '/customer/negotiable-quote?quoteid=': rootLink(CUSTOMER_NEGOTIABLE_QUOTE_PATH),
+  '/customer/negotiable-quote-template?quoteTemplateId=': rootLink(CUSTOMER_NEGOTIABLE_QUOTE_TEMPLATE_PATH),
+};
 
 /**
  * Renders the Company Switcher directly inline in the header navigation tools
@@ -19,10 +32,18 @@ export default async function renderCompanySwitcher(navTools) {
 
   await render.render(CompanySwitcher, {
     onCompanyChange: () => {
-      // first remove any query params from the url
-      window.history.replaceState({}, '', window.location.pathname);
-      // reload page
-      window.location.reload();
+      const redirect = Object.entries(redirections).find(([pattern]) => {
+        const [pathname, search] = pattern.split('?');
+        return window.location.pathname.includes(pathname)
+          && (!search || window.location.search.includes(search));
+      });
+
+      if (redirect) {
+        const [, redirectUrl] = redirect;
+        window.location.href = redirectUrl;
+      } else {
+        window.location.reload();
+      }
     },
   })(companySwitcherElement);
 

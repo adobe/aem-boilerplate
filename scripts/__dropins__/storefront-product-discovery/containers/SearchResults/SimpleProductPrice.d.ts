ed50f1335dde90fcd4bbde7516f8feb6e0921e1e
Dev Patil <40405790+devpatil7@users.noreply.github.com>
2025-12-12 15:34:13 -0600

    update discovery dropin (#998)
    * update discovery dropin
    
    * update to beta
    
    * sync with latest

---
+++ scripts/__dropins__/storefront-product-discovery/containers/SearchResults/SimpleProductPrice.d.ts
@@ -0,0 +1,6 @@
+import { Product } from '../../data/models/product';
+
+export declare const SimpleProductPrice: ({ product }: {
+    product: Product;
+}) => import("preact").JSX.Element;
+//# sourceMappingURL=SimpleProductPrice.d.ts.map

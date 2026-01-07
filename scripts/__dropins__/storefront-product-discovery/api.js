8317afa8571c9f07d6f50be2bea66722a8bb0053
Dev Patil <40405790+devpatil7@users.noreply.github.com>
2026-01-06 22:35:53 -0600

    update discovery dropin (#1010)
    * update discovery dropin
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- scripts/__dropins__/storefront-product-discovery/api.js
+++ scripts/__dropins__/storefront-product-discovery/api.js
@@ -1,4 +1,4 @@
-/*! Copyright 2025 Adobe
+/*! Copyright 2026 Adobe
 All Rights Reserved. */
 import{f as p,c as h,g as c,r as m,d,s as f,a as l,b as g}from"./chunks/search.js";import{c as G,i as Q}from"./chunks/initialize.js";import"@dropins/tools/lib.js";import"@dropins/tools/event-bus.js";import"./fragments.js";import"./chunks/acdlEvents.js";import"@dropins/tools/fetch-graphql.js";export{G as config,p as fetchGraphQl,h as getConfig,c as getFetchGraphQlHeader,Q as initialize,m as removeFetchGraphQlHeader,d as search,f as setEndpoint,l as setFetchGraphQlHeader,g as setFetchGraphQlHeaders};
 //# sourceMappingURL=api.js.map

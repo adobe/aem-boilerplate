d8ad71f2b4e7982038a8705d17cb7e38a103e5af
Jordi Ortolá Ankum <jortolaankum@adobe.com>
2026-01-14 21:43:39 +0100

    Bump payments dropin to v3 (#1056)

--- scripts/__dropins__/storefront-payment-services/api.js
+++ scripts/__dropins__/storefront-payment-services/api.js
@@ -1,4 +1,4 @@
-/*! Copyright 2025 Adobe
+/*! Copyright 2026 Adobe
 All Rights Reserved. */
 import{a as m,P as n,c as r,i as p}from"./chunks/location.js";import"@dropins/tools/lib.js";import"@dropins/tools/event-bus.js";import"@dropins/tools/signals.js";export{m as PaymentLocation,n as PaymentMethodCode,r as config,p as initialize};
 //# sourceMappingURL=api.js.map

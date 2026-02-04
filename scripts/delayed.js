1df69ae43dbd0df023fdf50f7750aae441c3374e
fkakatie <ksexton@adobe.com>
2026-02-04 14:01:39 -0800

    feat: open file links in new tabs automatically

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,27 @@
 // add delayed functionality here
+
+/**
+ * Decorates links to file types to open in new tabs.
+ * @param {Element} doc The container element
+ */
+function decorateFileLinks(doc) {
+  const fileExtensions = [
+    '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
+    '.pdf', '.zip', '.mp4', '.mov', '.avi',
+  ];
+  
+  doc.querySelectorAll('a[href]').forEach((link) => {
+    try {
+      const url = new URL(link.href);
+      const hasFileExtension = fileExtensions.some((ext) => url.pathname.toLowerCase().endsWith(ext));
+      
+      if (hasFileExtension && !link.hasAttribute('target')) {
+        link.setAttribute('target', '_blank');
+      }
+    } catch (e) {
+      // Invalid URL, skip
+    }
+  });
+}
+
+decorateFileLinks(doc);

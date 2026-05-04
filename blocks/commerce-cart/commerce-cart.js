b8ee27ce2ee6e25eb87722c6697ac3751f1305df
OscarMerino <merino@adobe.com>
2026-05-04 11:59:59 +0200

    fix: remove unused routeProduct prop from OrderSummary in commerce-cart (#1230)

--- blocks/commerce-cart/commerce-cart.js
+++ blocks/commerce-cart/commerce-cart.js
@@ -253,7 +253,6 @@ export default async function decorate(block) {
 
     // Order Summary
     provider.render(OrderSummary, {
-      routeProduct: createProductLink,
       routeCheckout: checkoutURL ? () => rootLink(checkoutURL) : undefined,
       slots: {
         EstimateShipping: async (ctx) => {
